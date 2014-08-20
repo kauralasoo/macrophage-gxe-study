1b1a490cd21d67dca55a5f0cfe34b8dc1d677df2
Kaur Alasoo <kaur.alasoo@gmail.com>
2014-08-20 19:17:59 +0100

    download raw data for DF

---
+++ df_fetch_raw_data.sh
@@ -0,0 +1,3 @@
+#20/08/14 - Download raw data from IRODS
+python ~/software/utils/fetch-irods.py --runid 13569 --laneids 5,6,7 --sampleids 1,2,3,4,5,6,7,8,9,10,10,11,12,13,14,15,16 --dir raw
+python ~/software/utils/fetch-irods.py --runid 13518 --laneids 8 --sampleids 1,2,3,4,5,6,7,8,9,10,10,11,12,13,14,15,16 --dir raw
