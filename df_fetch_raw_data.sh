7420098df6561a31ca0a573eb7a97d43f4a0655f
Kaur Alasoo <kaur.alasoo@gmail.com>
2014-08-20 21:01:45 +0100

    Download and merge bams

--- df_fetch_raw_data.sh
+++ df_fetch_raw_data.sh
@@ -1,3 +1,4 @@
 #20/08/14 - Download raw data from IRODS
-python ~/software/utils/fetch-irods.py --runid 13569 --laneids 5,6,7 --sampleids 1,2,3,4,5,6,7,8,9,10,10,11,12,13,14,15,16 --dir raw
-python ~/software/utils/fetch-irods.py --runid 13518 --laneids 8 --sampleids 1,2,3,4,5,6,7,8,9,10,10,11,12,13,14,15,16 --dir raw
+python ~/software/utils/fetch-irods.py --runid 13569 --laneids 5,6,7 --sampleids 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 --dir raw
+python ~/software/utils/fetch-irods.py --runid 13518 --laneids 8 --sampleids 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 --dir raw
+python ~/software/utils/renameFiles.py --samples macrophage-gxe-study/data/DF_rename_runs_patch1.txt --filedir raw --suffix .bam --execute True
