788bc64cfbdb1e9a176cb779baf813f6d68c8f8c
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-03-02 14:20:42 +0000

    Import and process raw RNA concentrations

---
+++ covariates/importRNAConcentrations.R
@@ -0,0 +1,32 @@
+library("dplyr")
+library("ggplot2")
+library("tidyr")
+
+rna_data = tbl_df(read.table("macrophage-gxe-study/data/RNA_extractions/RNA_extractions_170215.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE))
+
+#Select and process relevant columns
+rna_df = dplyr::rename(rna_data, ng_ul = ng.ul, ratio_260_280 = X260.280, ratio_260_230 = X260.230) %>% #Rename columns
+  dplyr::filter(!is.na(sample_id)) %>% #Keep non-NA samples
+  dplyr::select(sample_id, Date, ng_ul, ratio_260_280, ratio_260_230) %>% #Select only relevant columns
+  dplyr::mutate(Date = as.Date(Date, "%m/%d/%Y"))
+
+#Deal with know artifacts in the data
+rna_sample_stats = dplyr::mutate(rna_df, ng_ul = ifelse(grepl("eofe_",sample_id), 2*ng_ul, ng_ul)) %>% #1 well per condition
+  dplyr::mutate(ng_ul = ifelse(grepl("fpdl_",sample_id), 2*ng_ul, ng_ul)) %>% #1 well per condition
+  dplyr::mutate(ng_ul = ifelse(sample_id == "gomv_D", (21.5/35*ng_ul), ng_ul)) #gomv_D had 21.5 ul of sample instead of 35 ul
+
+#Separate sample_id into fields
+rna_sample_stats = tidyr::separate(rna_sample_stats, sample_id, into = c("donor","condition","replicate"), sep = "_", extra = "drop", remove = FALSE) %>%
+  mutate(replicate = ifelse(is.na(replicate), 1, replicate)) #if no replicate in file name then set it to 1
+
+#Calculate mean concentration per line
+rna_line_stats = dplyr::filter(rna_sample_stats, sample_id != "aipt_A") %>%
+  group_by(donor, replicate) %>%
+  summarise(ng_ul_mean = mean(ng_ul), extraction_date = Date[1]) %>%
+  ungroup()
+
+#Combine line-level and sample-level stats
+rna_stats = dplyr::left_join(rna_sample_stats, rna_line_stats, by = c("donor", "replicate"))
+saveRDS(rna_stats, "results/covariates/rna_concentrations.rds")
+write.table(rna_stats, "results/covariates/rna_concentrations.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
