a8c04992bd5b8047eeceb963964fc15f1662cb58
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-03-06 18:37:15 +0000

    Lift over SNPs from GRCh37 to GRCh38

---
+++ genotypes/postprocessCrossmap.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description = "Postprocess the VCF file created by the CrossMap to make it valid again.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--vcf", help = "Path to the VCF file.")
+args = parser.parse_args()
+
+vcf_file = open(args.vcf)
+contigs = dict()
+for line in vcf_file:
+	line = line.rstrip()
+	if(line[0] == "#"):
+		print(line)
+		if(line[0:8] == "##contig"):
+			contig = line.split("##contig=<ID=")[1].split(",length=")[0]
+			contigs[contig] = 1
+	else:
+		fields = line.split("\t",1)
+		fields[0] = "chr" + fields[0]
+		if(fields[0] in contigs):
+			print("\t".join(fields))
--- process_genotypes.sh
+++ process_genotypes.sh
@@ -1,24 +1,27 @@
 #Process genotyping data
 cut -f2 rna_seq_genptypes.txt | tail -n +2 > rna_seq_genotype_names.txt
 
-#Only keep the relevant genotypes from the vcf file
+#Only keep the relevant samples from the vcf file
 bcftools view -O z -S rna_seq_genotype_names.txt hipsci.wec.gtarray.HumanCoreExome-12_v1_0.858samples.20141111.genotypes.vcf.gz > hipsci.wec.gtarray.HumanCoreExome-12_v1_0.858samples.20141111.genotypes.subset.vcf.gz 
 
 #Only keep SNPs with MAF > 0.05
-bcftools filter -O z -i 'MAF[0] >= 0.05' hipsci.wec.gtarray.HumanCoreExome-12_v1_0.858samples.20141111.genotypes.subset.vcf.gz > hipsci.wec.gtarray.HumanCoreExome-12_v1_0.858samples.20141111.genotypes.subset.maf005.vcf.gz 
-
-#Convert vcf to bcf
-bcftools view -O b hipsci.wec.gtarray.HumanCoreExome-12_v1_0.858samples.20141111.genotypes.subset.maf005.vcf.gz > selected_genotypes.gz
+bcftools filter -i 'MAF[0] >= 0.05' hipsci.wec.gtarray.HumanCoreExome-12_v1_0.858samples.20141111.genotypes.subset.vcf.gz > selected_genotypes.vcf
 
 #Change the chromosome names of the bcf file
-bcftools annotate --rename-chrs GRCh38ToHg38_chromosome_map.txt -Ob selected_genotypes.bcf.gz > selected_genotypes.hg19.bcf.gz 
+bcftools annotate --rename-chrs GRCh38ToHg38_chromosome_map.txt selected_genotypes.vcf > selected_genotypes.hg19.vcf 
 
-#Construct dictionary of the hg38 ref sequence
-java -Xmx1000m -jar ~/software/picard-tools-1.129/picard.jar CreateSequenceDictionary R= hg38.fa O= hg38.dict
-java -Xmx1000m -jar ~/software/picard-tools-1.129/picard.jar CreateSequenceDictionary R= hg19.fa O= hg19.dict
+#Lift over variants from hg19 to hg38 (Only works on my MacBook Air)
+CrossMap.py vcf hg19ToHg38.over.chain selected_genotypes.hg19.vcf hg38.fa selected_genotypes.hg38.vcf
 
-#Convert BCF to VCF
-bcftools view selected_genotypes.hg19.bcf.gz > selected_genotypes.hg19.vcf
+#Fix chromosome names and remove ALT contigs introduced by CrossMap
+python ../macrophage-gxe-study/genotypes/postprocessCrossmap.py --vcf selected_genotypes.hg38.vcf > selected_genotypes.hg38.fixed.vcf
 
-#Lift over variants from hg19 to hg38
-/software/java/bin/java -Xmx1000m -jar ~/software/GenomeAnalysisTK.jar -T LiftoverVariants --newSequenceDictionary ../../../annotations/hg38/hg38.fa --chain hg19ToHg38.over.chain --out selected_genotypes.hg38.vcf --variant selected_genotypes.hg19.reordered.vcf -R ../../../annotations/hg19/hg19.fa 
+#Convert chromosome names back to GRCh38
+bcftools annotate --rename-chrs Hg38ToGRCh38_chromosome_map.txt selected_genotypes.hg38.fixed.vcf > selected_genotypes.GRCh38.vcf 
+
+#Sort vcf file by position
+grep '^#' selected_genotypes.GRCh38.vcf > selected_genotypes.GRCh38.sorted.vcf && grep -v '^#' selected_genotypes.GRCh38.vcf | LC_ALL=C sort -k1,1 -k2,2n >> selected_genotypes.GRCh38.sorted.vcf
+
+#Compress and index the VCF file
+bcftools view -O z selected_genotypes.GRCh38.sorted.vcf > selected_genotypes.GRCh38.sorted.vcf.gz
+bcftools index selected_genotypes.GRCh38.sorted.vcf.gz
