9efff1a470cecb1d6228d59ab6adc307fe0a642d
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-05-07 17:09:24 +0100

    Make report of variance components

--- SL1344/varComp/varianceComponents.R
+++ SL1344/varComp/varianceComponents.R
@@ -1,3 +1,4 @@
+#This script performs the timecomsuming bit of fitting mixed-effects models on normalized gene expression data
 library("plyr")
 library("dplyr")
 library("lme4")
@@ -54,48 +55,3 @@ saveRDS(var_table, "results/varComp/model_compact_results.rds")
 variance_list = lapply(gene_data_list, estimateVarianceExplained, model_extended)
 var_table = ldply(variance_list, .id = "gene_id")
 saveRDS(var_table, "results/varComp/model_extended_results.rds")
-
-
-
-
-
-
-
-#Analysed the compact model
-model_compact = readRDS("results/varComp/model_compact_results.rds") %>% tbl_df() %>%
-  dplyr::filter(converged == TRUE) %>%
-  dplyr::select(-type, -converged) %>% #Remove unnecessary columns
-  dplyr::rename(residual = Residual,SL1344_IFNg = `SL1344:IFNg`, batch = salmonella)
-
-#Bin by residual and calculate variance explained within each bin
-binned_table = binGenesByResidual(model_compact, n_bins = 20)
-var_explained = meanVarianceWithinBins(binned_table)
-var_comp_plot = plotBinnedVariance(var_explained) 
-ggsave("results/varComp/var_comp_plot.pdf", plot = var_comp_plot, width = 11, height = 7)
-
-#Make a violin plot of all components
-dat = tidyr::gather(model_compact, factor, var_explained, donor:SL1344_IFNg)
-ggplot(dat, aes(x = factor, y = var_explained)) + geom_violin(scale = "width" ) + geom_boxplot(width = .2, outlier.shape = NA)
-
-#### Pool technical source of variance together ####
-model_pooled = dplyr::transmute(model_compact, gene_id, donor, gender, IFNg, SL1344, SL1344_IFNg, residual, 
-                                technical = batch + library_pool + rna_concentration, purity = purity_bins)
-binned_pooled = binGenesByResidual(model_pooled, n_bins = 20)
-var_pooled = meanVarianceWithinBins(binned_pooled)
-var_pooled_plot = plotBinnedVariance(var_pooled)
-ggsave("results/varComp/var_pooled_plot.pdf", plot = var_pooled_plot, width = 11, height = 7)
-
-#Bin variance by maximum factor
-maximum_factor = maximumFactorPerGene(model_compact)
-ggplot(maximum_factor, aes(x = component_max, y = var_explained_max)) + geom_violin()
-table(maximum_factor$component_max)
-
-#Calculate the number of genes in each bin
-bin_size_table = dplyr::group_by(binned_table, residual_bin) %>% dplyr::summarise(bin_size = length(residual_bin))
-
-#Make plots
-bin_sizes_plot = ggplot(bin_size_table, aes(x = residual_bin, y = bin_size)) + geom_bar(stat = "identity") + 
-  ylab("Number of genes") + 
-  xlab("Residual variance bin")
-ggsave("results/varComp/bin_sizes_plot.pdf", plot = bin_sizes_plot, width = 8, height = 3)
-
