75c18c1aa8ffae5cd74fab7bc27a64840680be19
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-07-09 15:09:28 +0100

    Extract exon start and end coorinates from TxDd

--- SL1344/eQTL/prepareDataForRasqual.R
+++ SL1344/eQTL/prepareDataForRasqual.R
@@ -1,4 +1,6 @@
+library("plyr")
 library("dplyr")
+library("tidyr")
 library("devtools")
 library("GenomicFeatures")
 load_all("macrophage-gxe-study/seqUtils/")
@@ -11,9 +13,12 @@ line_metadata = readRDS("macrophage-gxe-study/data/covariates/compiled_line_meta
 line_metadata$line_id[c(which(line_metadata$line_id == "zuta_1"), which(line_metadata$line_id == "vorx_1"))] = c("vorx_1", "zuta_1")
 line_metadata$donor[c(which(line_metadata$donor == "zuta"), which(line_metadata$donor == "vorx"))] = c("vorx", "zuta")
 
+#Load transcript annotations
 txdb79 = loadDb("../../annotations/GRCh38/genes/TranscriptDb_GRCh38_79.db")
-gencode_basic_transcript = read.table("../../annotations/GRCh38/genes/Homo_sapiens.GRCh38.79.gencode_basic.txt", 
-                                      header = TRUE, stringsAsFactors = FALSE) %>% tbl_df()
+exons = exonsBy(txdb79, by = "tx", use.names = TRUE)
+gencode_basic_transcript = read.table("../../annotations/GRCh38/genes/Homo_sapiens.GRCh38.79.gencode_basic.txt", header = TRUE, stringsAsFactors = FALSE) %>% 
+  tbl_df() %>%
+  dplyr::rename(gene_id = ensembl_gene_id, transcript_id = ensembl_transcript_id)
 
 #Make the design matrix
 design = dplyr::filter(expression_dataset$design, !(donor == "ffdp")) %>% #ffdp identical to fpdl
@@ -36,10 +41,6 @@ write.table(dplyr::filter(sg_match, condition == "C") %>% dplyr::select(-conditi
 write.table(dplyr::filter(sg_match, condition == "D") %>% dplyr::select(-condition),
             "genotypes/SL1344/SL1344_sg_map_D.txt", row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
 
-#Extract exon start and end coordinates from txdb 
-exons = exonsBy(txdb79, by = "tx", use.names = TRUE)
-gene_ids = unique(sort(gencode_basic_transcript$ensembl_gene_id))
-gene_id_list = as.list(gene_ids)
-names(gene_id_list) = gene_ids
-
-gene_exons = lapply(gene_id_list, findGeneExons, gencode_basic_transcript, exons)
+#Extract exon start and end coordinates from txdb
+gene_exon_start_end = extractExonsStartEnd(exons, gencode_basic_transcript)
+write.table(gene_exon_start_end, "annotations/Homo_sapiens.GRCh38.79.gene_exon_start_end.txt", row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
--- seqUtils/R/processGeneAnnotations.R
+++ seqUtils/R/processGeneAnnotations.R
@@ -9,10 +9,69 @@ listUnion <- function(granges_list){
   return(union_obj)
 }
 
-findGeneExons <- function(gene_id, gene_tx_map, tx_exon_list){
-  tx_vector = gene_tx_map[gene_tx_map$ensembl_gene_id == gene_id,]$ensembl_transcript_id
-  selected_txs = tx_exon_list[tx_vector]
-  gene_exons = listUnion(selected_txs)
-  return(gene_exons)
+
+reduceDataFrameGRanges <- function(df){
+  #Convert data frame into a a Granges object and reduce overlapping exons, then convert back to df
+  
+  gr = GRanges(seqnames = df$seqnames, ranges = IRanges(start = df$start, end = df$end), strand = df$strand)
+  #Reduce exons
+  gr = IRanges::reduce(gr)
+  #Convert back to data.frame
+  gr = GenomicRanges::as.data.frame(gr)
+  return(gr)
 }
 
+GRangesListToDataFrame <- function(gr_list, joint_id_df){
+  #Convert GRangesList into a data frame of exon coordinates
+  # gr_list - GRangesList with tr_ids as names
+  # joint_id_df - data.frame(with rownames) containing three columns: gene_id, transcript_id and joint_id
+  
+  names(gr_list) = joint_id_df[names(gr_list),]$joint_id
+  exons_df = plyr::ldply(gr_list, GenomicRanges::as.data.frame, .id = "joint_id") %>%
+    dplyr::mutate(joint_id = as.vector(joint_id)) %>%
+    tidyr::separate(joint_id, into = c("gene_id", "transcript_id"), sep = ":")
+  return(exons_df)
+}
+
+extractExonsStartEnd <- function(gr_list, gene_tx_map){
+  #Extract Exon start and end coorinates per gene from a GrangesList object of exons by transcript id.
+  
+  #Count the number of transcripts per gene
+  transcript_counts = dplyr::group_by(gene_tx_map, gene_id) %>% 
+    dplyr::summarize(transcript_count = length(transcript_id))
+  
+  #Extract genes with only one transcipt
+  single_transcript_genes = dplyr::filter(transcript_counts, transcript_count == 1)
+  print(dim(single_transcript_genes))
+  single_tx_annot = dplyr::semi_join(gene_tx_map, single_transcript_genes, by = "gene_id")
+  
+  #Extract genes with more than one transcript
+  multi_transcript_genes = dplyr::filter(transcript_counts, transcript_count > 1)
+  print(dim(multi_transcript_genes))
+  multi_tx_annot = dplyr::semi_join(gene_tx_map, multi_transcript_genes, by = "gene_id")
+  
+  #Construct joint id for multi tx genes
+  joint_id_df = dplyr::mutate(gene_tx_map, joint_id = paste(gene_id, transcript_id, sep = ":")) %>% as.data.frame()
+  rownames(joint_id_df) = joint_id_df$transcript_id
+  
+  #Extract exon coordinates for single tx genes
+  single_tx_exons = exons[single_tx_annot$transcript_id]
+  single_tx_exons_df = GRangesListToDataFrame(single_tx_exons, joint_id_df)
+  single_tx_exons_coords = dplyr::group_by(single_tx_exons_df, gene_id) %>% 
+    arrange(start) %>% 
+    summarize(starts = paste(start, collapse = ","), ends = paste(end, collapse = ","))
+  
+  #Extract joint exon coordinates for multi tx genes
+  multi_tx_exons = exons[multi_tx_annot$transcript_id]
+  multi_tx_exons_df = GRangesListToDataFrame(multi_tx_exons, joint_id_df)
+  multi_tx_exons_reduced_df = dlply(multi_tx_exons_df, .(gene_id), seqUtils::reduceDataFrameGRanges) %>%
+    ldply(.id = "gene_id")
+  multi_tx_exons_coords = dplyr::group_by(multi_tx_exons_reduced_df, gene_id) %>% 
+    arrange(start) %>% 
+    summarize(starts = paste(start, collapse = ","), ends = paste(end, collapse = ","))
+  
+  #bind results together
+  result = rbind(single_tx_exons_coords, multi_tx_exons_coords)
+  return(result)
+  
+}
