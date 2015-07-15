4836042846d6c9ad424aa455762899e42c9f07bb
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-07-15 18:42:48 +0100

    Correct vorx <-> zuta swap in acLDL data

--- acLDL/QC/processVerifyBamID.R
+++ acLDL/QC/processVerifyBamID.R
@@ -6,4 +6,5 @@ load_all("macrophage-gxe-study/seqUtils/")
 sample_names = read.table("macrophage-gxe-study/data/sample_lists/acLDL/acLDL_names_all.txt", sep ="\t",comment.char = "", stringsAsFactors = FALSE)[,1]
 verify_bam_id = loadVerifyBamID(sample_names, "STAR/acLDL")
 
-write.table(verify_bam_id, "macrophage-gxe-study/data/sample_lists/acLDL/acLDL_true_genotypes.txt")
+write.table(verify_bam_id, "macrophage-gxe-study/data/sample_lists/acLDL/acLDL_true_genotypes.txt", 
+            sep = "\t", row.names = FALSE, quote = FALSE)
