0334ee2f3f4ad5e0bc9e57184318bc792a2bc4d1
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-08-18 14:07:09 +0100

    First try on differential accessibility

--- munge/combinePeakCounts.R
+++ munge/combinePeakCounts.R
@@ -1,11 +1,19 @@
 library("rtracklayer")
 library("devtools")
 library("dplyr")
+library("gplots")
 load_all("../macrophage-gxe-study/macrophage-gxe-study/seqUtils/")
 
 #Import sample names
 sample_names = read.table("macrophage-chromatin/data/SL1344/ATAC_Salmonella_names.txt", sep ="\t",comment.char = "", stringsAsFactors = FALSE)[,1]
 
-a = loadCounts("processed/SL1344/", sample_names[1:5], counts_suffix = ".consensus_peaks.counts.txt")
+atac_counts = loadCounts("processed/SL1344/", sample_names, counts_suffix = ".consensus_peaks.counts.txt")
 
-heatmap.2(cor(a[,3:ncol(a)],method = "spearman"))
+#Make heatmap
+pdf("results/ATAC/QC/ATAC_spearman_heatmap.pdf", width = 10, height = 10)
+heatmap.2(cor(atac_counts[,3:ncol(atac_counts)],method = "spearman"), margins = c(10,10), tracecol = NA)
+dev.off()
+
+#Save counts to disk
+write.table(atac_counts, "results/ATAC/ATAC_combined_counts.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+saveRDS(atac_counts, "results/ATAC/ATAC_combined_counts.rds")
