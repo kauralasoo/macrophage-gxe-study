a695e41a3c44dfafadb10fc44c3ea777b90172fd
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-09-05 16:05:29 +0100

    test eqtlbma

--- acLDL/DE/performDE.R
+++ acLDL/DE/performDE.R
@@ -4,7 +4,7 @@ library("devtools")
 library("gplots")
 library("ggplot2")
 library("tidyr")
-load_all("macrophage-gxe-study/seqUtils/")
+load_all("../seqUtils/")
 
 #Load processed expression data from disk
 dataset = readRDS("results/acLDL/acLDL_combined_expression_data.rds")
@@ -29,7 +29,6 @@ cor_matrix = cor(cqn_expressed, method = "spearman")
 heatmap.2(cor_matrix, margins = c(12,12))
 
 #Perform PCA
-tpm_z = zScoreNormalize(tpm_expressed)
 pca_list = performPCA(cqn_expressed, dataset$design)
 ggplot(pca_list$pca_matrix, aes(x = PC1, y = PC2, color = condition, label = sample_id)) + 
   geom_point() +
@@ -47,6 +46,14 @@ de_results = res %>%
   dplyr::filter(padj < 0.05, abs(log2FoldChange) > 1) %>% 
   dplyr::arrange(desc(log2FoldChange))
 
+#Don't apply any fold-change filtering
+de_results_unfiltered = res %>%
+  as.data.frame() %>% #Convert to data.frame
+  dplyr::mutate(gene_id = rownames(res)) %>% #Add gene id as a column
+  dplyr::left_join(gene_data, by = "gene_id") %>% #AD
+  tbl_df()
+
 #Save results to disk
-write.table(de_results, "results/acLDL/DE_genes.txt", sep= "\t", quote = FALSE, row.names = FALSE)
+write.table(de_results, "results/acLDL/DE/DE_genes.txt", sep= "\t", quote = FALSE, row.names = FALSE)
+write.table(de_results_unfiltered, "results/acLDL/DE/DE_genes_unfiltered.txt", sep= "\t", quote = FALSE, row.names = FALSE)
 write.table(filtered_exprs_cqn, "results/acLDL/expression_cqn_normalised.txt", sep = "\t", quote = FALSE)
