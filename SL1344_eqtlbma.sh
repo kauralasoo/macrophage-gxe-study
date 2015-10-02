ff2e05d0049cc95f9ac8ce8024a192a3d47e8bda
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-10-02 18:34:03 +0100

    Run eqtlbma in batches

--- SL1344_eqtlbma.sh
+++ SL1344_eqtlbma.sh
@@ -18,11 +18,17 @@ eqtlbma_avg_bfs --in eqtlbma/output/subset_test_l10abfs_raw.txt.gz --gwts eqtlbm
 
 
 #Run eqtlbma with covariates
+#Split genes into batches
+/software/R-3.1.2/bin/Rscript ~/software/utils/eqtlbma/splitGeneCoords.R -i eqtlbma/input/gene_coords.bed -b 200 -o eqtlbma/input/gene_batches.txt
+
 #Compute bayes factors for each SNP/gene pair
-bsub -G team170 -n1 -R "span[hosts=1] select[mem>4000] rusage[mem=4000]" -q normal -M 4000 -o FarmOut/eqtlbma_bf.%J.jobout "eqtlbma_bf --geno eqtlbma/input/list.genotypes.txt --scoord eqtlbma/input/snp_coords.bed.gz --exp eqtlbma/input/list.expression.txt --gcoord eqtlbma/input/gene_coords.bed --anchor TSS --cis 500000 --out eqtlbma/output/out_eqtlbma_full --analys join --covar eqtlbma/input/list.covariates.txt --gridL macrophage-gxe-study/data/eqtlbma/grid_phi2_oma2_general.txt  --gridS macrophage-gxe-study/data/eqtlbma/grid_phi2_oma2_with-configs.txt --bfs all --error mvlr -v 3"
+cat eqtlbma/input/gene_batches.txt | python ~/software/utils/submitJobs.py --MEM 2000 --jobname run_eqtlbma_bf --command  "python ~/software/utils/eqtlbma/run_eqtlbma_bf.py --indir eqtlbma/input/ --outdir eqtlbma/output/ --outprefix eqtlbma_batched"
+
+#Merge results from all batches
+python ~/software/utils/eqtlbma/merge_eqtlbma_bf.py --geneBatches eqtlbma/input/gene_batches.txt --outprefix eqtlbma_batched --outdir eqtlbma/output/
 
 #Run the hierarchical model
-bsub -G team170 -n1 -R "span[hosts=1] select[mem>6000] rusage[mem=6000]" -q normal -M 6000 -o FarmOut/eqtlbma_hm.%J.jobout "python ~/software/utils/run_eqtlbma_hm.py --workdir eqtlbma/output/ --outprefix out_eqtlbma_full --nsubgrp 4 --dim 15"
+bsub -G team170 -n1 -R "span[hosts=1] select[mem>12000] rusage[mem=12000]" -q normal -M 12000 -o FarmOut/eqtlbma_hm.%J.jobout "python ~/software/utils/eqtlbma/run_eqtlbma_hm.py --workdir eqtlbma/output/ --outprefix eqtlbma_batched --nsubgrp 4 --dim 15"
 
 #Run the EBF procedure
 bsub -G team170 -n1 -R "span[hosts=1] select[mem>8000] rusage[mem=8000]" -q normal -M 8000 -o FarmOut/eqtlbma_EBF.%J.jobout "eqtlbma_avg_bfs --in 'eqtlbma/output/out_eqtlbma_full_l10abfs_raw.txt.gz' --gwts eqtlbma/output/out_eqtlbma_full_grid_weights.txt --nsubgrp 4 --dim 15 --cwts eqtlbma/output/out_eqtlbma_full_config_weights.txt --save bf --out eqtlbma/output/out_eqtlbma_avg_bfs_genes.txt.gz"
