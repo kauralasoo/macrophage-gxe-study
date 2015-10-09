1f6d6725d2d987a540ea0c60848d37613f9bf92f
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-10-09 18:45:32 +0100

    eqtlbma explore

--- SL1344/eqtlbma/exploreEqtlbmaResults.R
+++ SL1344/eqtlbma/exploreEqtlbmaResults.R
@@ -5,12 +5,13 @@ load_all("../seqUtils/")
 library("ggplot2")
 load_all("macrophage-gxe-study/housekeeping/")
 library("tidyr")
+library("lme4")
 
 #Load the raw eQTL dataset
 eqtl_data_list = readRDS("results/SL1344/eqtl_data_list.rds")
 expression_dataset = readRDS("results/SL1344/combined_expression_data.rds") #expression data
 line_metadata = readRDS("macrophage-gxe-study/data/covariates/compiled_line_metadata.rds") #Line metadata
-vcf_file = readRDS("genotypes/SL1344/array_genotypes.59_samples.vcfToMatrix.rds") #genotypes
+vcf_file = readRDS("genotypes/SL1344/array_genotypes.59_samples.imputed.vcfToMatrix.rds") #genotypes
 gene_id_name_map = dplyr::select(eqtl_data_list$gene_metadata, gene_id, gene_name)
 
 #Filter the expression data set
@@ -20,7 +21,7 @@ expression_dataset$exprs_cqn = expression_dataset$exprs_cqn[,selected_samples]
 expression_dataset$exprs_counts = expression_dataset$exprs_counts[,selected_samples]
 
 #Import eqtlbma results
-results = read.table(gzfile("eqtlbma/output/eqtlbma_batched_avg_bfs.txt.gz"), sep = "\t", skip = 1, header = TRUE, stringsAsFactors = FALSE)
+results = read.table(gzfile("eqtlbma/output_imputed//eqtlbma_imputed_avg_bfs.txt.gz"), sep = "\t", skip = 1, header = TRUE, stringsAsFactors = FALSE)
 significant_genes = dplyr::filter(results, gene.post > 0.8) %>% tbl_df() %>%
   dplyr::select(gene, snp, gene.post, snp.post.the, snp.post.an, best.config, post.best.config) %>%
   dplyr::group_by(gene) %>%
@@ -31,24 +32,24 @@ significant_genes = dplyr::filter(results, gene.post > 0.8) %>% tbl_df() %>%
 table(significant_genes$best.config)
 
 #Make plots for condition-specific eQTLs
-inflammatory_qtls = dplyr::filter(significant_genes, best.config == "1-2-3")
-inflammatory_plots = makeMultiplePlots(inflammatory_qtls, expression_dataset, vcf_file, line_metadata)
-savePlots(inflammatory_plots, path = "results/SL1344/eqtlbma/inflammatory_plots/",width = 7, height = 7)
+dplyr::filter(significant_genes, best.config == "1-2-3") %>%
+  makeMultiplePlots(.,expression_dataset, vcf_file, line_metadata) %>%
+savePlots(.,path = "results/SL1344/eqtlbma/inflammatory_plots/",width = 7, height = 7)
 
 #Make plots for condition-specific eQTLs
-ifng_qtls = dplyr::filter(significant_genes, best.config %in% c("1-2","1"))
-ifng_plots = makeMultiplePlots(ifng_qtls, expression_dataset, vcf_file, line_metadata)
-savePlots(ifng_plots, path = "results/SL1344/eqtlbma/ifng_plots/",width = 7, height = 7)
+dplyr::filter(significant_genes, best.config %in% c("1-2","1")) %>%
+  makeMultiplePlots(., expression_dataset, vcf_file, line_metadata) %>%
+  savePlots(., path = "results/SL1344/eqtlbma/ifng_plots/",width = 7, height = 7)
 
 #Salmonella specific
-SL1344_qtls = dplyr::filter(significant_genes, best.config == "2-3")
-SL1344_plots = makeMultiplePlots(SL1344_qtls, expression_dataset, vcf_file, line_metadata)
-savePlots(SL1344_plots, path = "results/SL1344/eqtlbma/SL1344_plots/",width = 7, height = 7)
+dplyr::filter(significant_genes, best.config == "2-3") %>%
+  makeMultiplePlots(., expression_dataset, vcf_file, line_metadata) %>%
+  savePlots(., path = "results/SL1344/eqtlbma/SL1344_plots/",width = 7, height = 7)
 
 # Not Salmonella specific
-not_SL1344_qtls = dplyr::filter(significant_genes, best.config %in% c("1-4","1-3-4"))
-not_SL1344_plots = makeMultiplePlots(not_SL1344_qtls, expression_dataset, vcf_file, line_metadata)
-savePlots(not_SL1344_plots, path = "results/SL1344/eqtlbma/not_SL1344_plots/",width = 7, height = 7)
+dplyr::filter(significant_genes, best.config %in% c("1-4","1-3-4")) %>%
+  makeMultiplePlots(., expression_dataset, vcf_file, line_metadata) %>%
+  savePlots(., path = "results/SL1344/eqtlbma/not_SL1344_plots/",width = 7, height = 7)
 
 #Test for interactions between lead SNP and condition
 res = testMultipleInteractions(significant_genes, eqtl_data_list)
@@ -125,3 +126,26 @@ dplyr::filter(best_config, best_config == "IFNg_SL1344") %>%
 dplyr::filter(best_config, best_config == "not_IFNg_SL1344") %>%
   makeMultiplePlots(.,expression_dataset, vcf_file, line_metadata) %>%
   savePlots(.,path = "results/SL1344/eqtlbma/two-step/not_IFNg_SL1344/",width = 7, height = 7)
+
+
+#Play around with lmer4
+golga7_data = constructGeneData("ENSG00000147533","rs6998193", eqtl_data_list)
+lmer2_model = lmer(expression ~  genotype + (1+genotype|condition_name), golga7_data, REML = FALSE)
+lmer1_model = lmer(expression ~  genotype + (1|condition_name), golga7_data, REML = FALSE)
+a = anova(lmer1_model, lmer2_model)
+
+
+golga7_data = constructGeneData("ENSG00000170458","rs778583", eqtl_data_list)
+lmer2_model = lmer(expression ~  genotype + (1+genotype|condition_name), golga7_data, REML = FALSE)
+lmer1_model = lmer(expression ~  genotype + (1|condition_name), golga7_data, REML = FALSE)
+a = anova(lmer1_model, lmer2_model)
+
+lmer3_model = lmer(expression ~ (1|condition_name), golga7_data, REML = FALSE)
+
+lmer1_model = lmer(expression ~  genotype + (1+genotype|condition_name), golga7_data, REML = FALSE)
+lmer2_model = lmer(expression ~  (1+genotype|condition_name), golga7_data, REML = FALSE)
+anova(lmer1_model, lmer2_model)
+
+lm1_model = lm(expression ~  genotype, golga7_data)
+lm2_model = lm(expression ~ 1, golga7_data)
+anova(lm1_model, lm2_model)
