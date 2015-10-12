9685f58246e96d47ae87a3819e52d27710d06995
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-10-12 01:24:01 +0100

    make flowQTL boxplots

--- flow/flowVarianceComponents.R
+++ flow/flowVarianceComponents.R
@@ -26,8 +26,11 @@ flow_data = dplyr::left_join(flow_purity, channel_marker_map, by = "channel") %>
   dplyr::select(line_id, genotype_id, donor, flow_date, marker, purity, intensity)
 
 #Import genotypes
-a = vcfToMatrix("CD14_qtl.vcf", "GRCh38", genotype_sep = "/")
-cd14_qtl = dplyr::mutate(as.data.frame(t(a$genotypes)), genotype_id = colnames(a$genotypes))
+GWASTools::convertVcfGds("CD14_qtl.vcf", "CD14_qtl.gds")
+gds <- GdsGenotypeReader("CD14_qtl.gds")
+cd14_qtl = data_frame(genotype_id = getVariable(gds, "sample.name"), gt = getGenotype(gds))
+colnames(cd14_qtl)[colnames(cd14_qtl) == "gt"] = getVariable(gds, "snp.rs.id")
+close(gds)
 
 #CD16 (FCGR3B)
 #"bcftools view -r 1:161732020 -S macrophage-gxe-study/data/sample_lists/flow_cytometry_gt_list.txt genotypes/GRCh38/imputed_20151005/hipsci.wec.gtarray.HumanCoreExome-12_v1_0.REL-2014-11.imputed_phased.INFO_0.4_filtered.20151005.genotypes.chr1.GRCh38.sorted.vcf.gz > FCGR3B_qtl.vcf"
@@ -35,14 +38,20 @@ GWASTools::convertVcfGds("FCGR3B_qtl.vcf", "FCGR3B_qtl.gds")
 gds <- GdsGenotypeReader("FCGR3B_qtl.gds")
 cd16_qtl = data_frame(genotype_id = getVariable(gds, "sample.name"), gt = getGenotype(gds))
 colnames(cd16_qtl)[colnames(cd16_qtl) == "gt"] = getVariable(gds, "snp.rs.id")
+close(gds)
 
 #Analyse CD14 only
 cd14_data = dplyr::filter(flow_data, marker == "CD14") %>%
   dplyr::left_join(cd14_qtl, by = "genotype_id")
-cd14_model = lmer(intensity ~ (1|flow_date) + (1|donor), cd14_data)
-cd14_variance = seqUtils::varianceExplained(cd14_model)
-cd14_model = lmer(intensity ~ (1|flow_date) + (1|donor) + (1|rs778583), cd14_data)
-cd14_variance = seqUtils::varianceExplained(cd14_model)
+cd14_model_1 = lmer(intensity ~ (1|flow_date) + (1|donor), cd14_data)
+cd14_variance = seqUtils::varianceExplained(cd14_model1)
+cd14_model_2 = lmer(intensity ~ (1|flow_date) + (1|donor) + (1|rs778583), cd14_data)
+cd14_variance = seqUtils::varianceExplained(cd14_model_2)
+
+#Make a QTL plot
+cd14_qtl_plot = ggplot(cd14_data, aes(x = factor(rs778583), y = intensity)) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(position = position_jitter(width = .1))
 
 
 #Analyse CD16 only
@@ -52,6 +61,12 @@ cd16_model_1 = lmer(intensity ~ (1|flow_date) + (1|donor), cd16_data, REML = TRU
 cd16_variance_1 = seqUtils::varianceExplained(cd16_model_1)
 cd16_model_2 = lmer(intensity ~ (1|flow_date) + (1|donor) + (1|rs10917809), cd16_data, REML = TRUE)
 cd16_variance_2 = seqUtils::varianceExplained(cd16_model_2)
+
+#Make boxplot
+cd16_qtl_plot = ggplot(cd16_data, aes(x = factor(rs10917809), y = intensity)) +
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(position = position_jitter(width = .1))
+
 
 #Analyse CD206 only
 cd206_data = dplyr::filter(flow_data, marker == "CD206")
