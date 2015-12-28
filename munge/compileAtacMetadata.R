b3d89c0f51d19764a60786c2224d2d99eefa9075
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-12-28 09:49:31 +0200

    Add QC metrics into the metadata table

--- munge/compileAtacMetadata.R
+++ munge/compileAtacMetadata.R
@@ -25,6 +25,27 @@ atac_metadata = dplyr::mutate(atac_metadata,
             max_purity_filtered = ifelse(as.numeric(flow_date - stimulation_date) > 14, NA, max_purity),
             mean_purity_filtered = ifelse(as.numeric(flow_date - stimulation_date) > 14,NA,mean_purity))
 
+#Add QC metrics calculated from seq data
+assigned = read.table("macrophage-chromatin/data/SL1344/QC_measures/ATAC_assigned_fraction.txt", 
+                      header = TRUE, stringsAsFactors = FALSE) %>% 
+  dplyr::transmute(sample_id, assigned = Assigned, assigned_frac)
+mt_fraction = read.table("macrophage-chromatin/data/SL1344/QC_measures/ATAC_mitochondrial_fraction.txt",
+           header = TRUE, stringsAsFactors = FALSE) %>%
+  dplyr::transmute(sample_id, mt_frac = MT)
+dupl_fraction = read.table("macrophage-chromatin/data/SL1344/QC_measures/ATAC_duplication_fraction.txt",
+                           header = TRUE, stringsAsFactors = FALSE)
+short_long_ratio = read.table("macrophage-chromatin/data/SL1344/QC_measures/ATAC_short_long_ratio.txt",
+                           header = TRUE, stringsAsFactors = FALSE) %>%
+  dplyr::transmute(sample_id, short_long_ratio = length_ratio)
+peak_count = read.table("macrophage-chromatin/data/SL1344/QC_measures/macs2_peaks_counts.txt",
+                        header = TRUE, stringsAsFactors = FALSE)
+
+atac_metadata = dplyr::left_join(atac_metadata, assigned, by = "sample_id") %>%
+  dplyr::left_join(mt_fraction, by = "sample_id") %>%
+  dplyr::left_join(dupl_fraction, by = "sample_id") %>%
+  dplyr::left_join(short_long_ratio, by = "sample_id") %>%
+  dplyr::left_join(peak_count, by = "sample_id")
+
 #Save results to disk
 saveRDS(atac_metadata, "macrophage-chromatin/data/SL1344/compiled_atac_metadata.rds")
 write.table(atac_metadata, "macrophage-chromatin/data/SL1344/compiled_atac_metadata.txt", 
