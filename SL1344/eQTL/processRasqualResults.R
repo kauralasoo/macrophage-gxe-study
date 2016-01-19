4f15fee1a04f3b73971fcdc0b327c8eb466d5f96
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-01-19 18:48:01 +0000

    Remove old scripts

--- SL1344/eQTL/processRasqualResults.R
+++
@@ -1,7 +0,0 @@
-labels = c("gene_id","snp_id","chromosome", "snp_pos", "REF","ALT","AAF","HWE","IA",
-           "R2","chi_sq","Pi","Phi","Delta", "OD","snp_id_region","fSNP_count",
-           "tSNP_count","null_iter","alt_iter","ties","null_lik","convergence",
-           "fSNP_corr","rSNP_corr")
-cd14 = read.table("rasqual/input/CD14.txt")
-colnames(cd14) = labels
-plot(cd14$snp_pos,cd14$chi_sq)
