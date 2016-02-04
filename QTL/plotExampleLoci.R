50d3d022ac4f5bb5dfa1c1861432c373cabd502b
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-02-04 11:35:52 +0000

    Make a coverage plot fo the SPOPL enhancer

---
+++ QTL/plotExampleLoci.R
@@ -0,0 +1,43 @@
+library("devtools")
+load_all("../seqUtils/")
+load_all("../wiggleplotr/")
+library("GenomicRanges")
+library("dplyr")
+library("ggplot2")
+
+#Import data
+atac_list = readRDS("../macrophage-chromatin/results/ATAC/ATAC_combined_accessibility_data.rds")
+
+#Import genotypes
+vcf_file = readRDS("../macrophage-gxe-study/genotypes/SL1344/imputed_20151005/imputed.86_samples.sorted.filtered.named.rds")
+
+#SPOPL enhancer
+selected_peaks = dplyr::filter(atac_list$gene_metadata, gene_id %in% c("ATAC_peak_154955", "ATAC_peak_154957", "ATAC_peak_154958"))
+selected_peaks = dplyr::filter(atac_list$gene_metadata, start > 138680000, end < 138713000, chr == 2)
+peak_list = list(ATAC = dplyr::transmute(selected_peaks, seqnames = chr, start, end, strand) %>% dataFrameToGRanges())
+peak_annot = data_frame(transcript_id = "ATAC", gene_id = "ATAC", gene_name = "ATAC-seq", strand = "+")
+
+#Sample sizes
+sample_sizes = data_frame(sample_id = colnames(atac_list$counts), scaling_factor = colSums(atac_list$counts)/1e6)
+
+#Extract genotypes
+genotype_df = vcf_file$genotypes["rs12621644",] %>% tidyVector(sample_id = "genotype_id", value_id = "genotype")
+
+#Construct meta-data
+plotting_meta = atac_list$sample_metadata %>%
+  dplyr::select(sample_id, genotype_id, condition_name) %>%
+  dplyr::mutate(bigWig = file.path("processed/SL1344", sample_id, paste(sample_id, ".bw", sep = ""))) %>%
+  dplyr::mutate(track_id = factor(condition_name, levels = c("naive","IFNg", "SL1344", "IFNg_SL1344"))) %>%
+  dplyr::left_join(sample_sizes, by = "sample_id") %>%
+  dplyr::left_join(genotype_df, by = "genotype_id") %>%
+  dplyr::mutate(colour_group = factor(genotype, levels = c(2,1,0)))
+
+filtered_tracks = dplyr::filter(plotting_meta, track_id %in% c("naive", "IFNg"))
+
+#Make plot
+spopl_region = plotCoverage(exons = peak_list, cdss = peak_list, track_data = filtered_tracks, rescale_introns = FALSE, 
+                transcript_annotations = peak_annot, fill_palette = c("#a1dab4","#41b6c4","#225ea8"), flanking_length = c(300,300), 
+                connect_exons = FALSE, label_type = "peak", plot_fraction = 0.2)
+ggsave("results/ATAC/QTLs/figures/SPOPL_region.pdf", spopl_region, width = 10, height = 8)
+
+
