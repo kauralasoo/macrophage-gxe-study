66753a2767b4616ae0cbcf76e0f495037ee82425
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-02-04 19:09:52 +0000

    Rerun failed batches

--- QTL/exportDataForQTLMapping.R
+++ QTL/exportDataForQTLMapping.R
@@ -157,6 +157,17 @@ IFNg_failed_batches = readr::read_csv("results/ATAC/rasqual/output/IFNg_100kb/IF
 write.table(IFNg_failed_batches, "results/ATAC/rasqual/input/IFNg_failed_batches.txt", 
             row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
 
-IFNg_failed_batches = readr::read_csv("results/ATAC/rasqual/output/SL1344_100kb/SL1344_100kb.completed_ids.txt", col_names = "gene_id") %>%
+SL1344_failed_batches = readr::read_csv("results/ATAC/rasqual/output/SL1344_100kb/SL1344_100kb.completed_ids.txt", col_names = "gene_id") %>%
   dplyr::anti_join(peak_snp_count_100kb, ., by = "gene_id") %>%
   rasqualOptimisedGeneBatches(c(10,10,1,1))
+write.table(SL1344_failed_batches, "results/ATAC/rasqual/input/SL1344_failed_batches.txt", 
+            row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+IFNg_SL1344_failed_batches = readr::read_csv("results/ATAC/rasqual/output/IFNg_SL1344_100kb/IFNg_SL1344_100kb.completed_ids.txt", col_names = "gene_id") %>%
+  dplyr::anti_join(peak_snp_count_100kb, ., by = "gene_id") %>%
+  rasqualOptimisedGeneBatches(c(10,10,1,1))
+write.table(IFNg_SL1344_failed_batches, "results/ATAC/rasqual/input/IFNg_SL1344_failed_batches.txt", 
+            row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+#Export ATAC peak positions for eigenMT
+eigenMTExportGeneMetadata(atac_list$gene_metadata, "results/ATAC/eigenMT/input")
