5df5514addbe434d95a6b22431e082175c783b6f
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-03-22 15:38:34 +0200

    Import cisBP motifs

--- munge/importCisBPDatabase.R
+++ munge/importCisBPDatabase.R
@@ -12,9 +12,23 @@ TF_information = readr::read_tsv("~/annotations/CisBP/Homo_sapiens_2016_03_10_11
 colnames(TF_information)[6] = "gene_id"
 unique_motifs = dplyr::select(TF_information, Motif_ID, gene_id, TF_Name) %>% dplyr::filter(Motif_ID != ".")
 
-read.table("~/annotations/CisBP/Homo_sapiens_2016_03_10_11-59_am/pwms_all_motifs/M0082_1.02.txt", header = TRUE)
+#Filter motifs by TF expression
+combined_expression_data = readRDS("../macrophage-gxe-study/results/SL1344/combined_expression_data.rds")
+mean_tpm = calculateMean(combined_expression_data$tpm, as.data.frame(combined_expression_data$sample_metadata), "condition_name")
+expressed_genes = names(which(apply(mean_tpm, 1, max) > 1))
+expressed_motifs = dplyr::filter(unique_motifs, gene_id %in% expressed_genes)
 
-a = cisBPImportMotif("M0082_1.02","~/annotations/CisBP/Homo_sapiens_2016_03_10_11-59_am/pwms_all_motifs/")
+#Collapse motifs
+motifs_collapsed = dplyr::group_by(expressed_motifs, Motif_ID) %>% 
+  dplyr::summarise(gene_id = paste(gene_id, collapse = ";"), TF_Name = paste(TF_Name,collapse = ";"), TF_count = length(Motif_ID))
+
+#Import PWMs
+cisbp_dir = "~/annotations/CisBP/Homo_sapiens_2016_03_10_11-59_am/pwms_all_motifs/"
+pwmatrix_list = cisBPImportRecords(motifs_collapsed, cisbp_dir)
+
+#Search for matches
+sitesetList = searchSeq(pwmatrix_list, subject, seqname="seq1", min.score="80%", strand="*")
+a = as.data.frame(sitesetList)
 
 
-motif_record = unique_motifs[10,]
+
