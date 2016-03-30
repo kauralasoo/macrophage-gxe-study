4b77798df689c155d323d6a1b1d32522a091a42f
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-03-30 17:38:00 +0100

    Import fastqtl results

--- QTL/rasqual_min_pvalues.R
+++ QTL/rasqual_min_pvalues.R
@@ -3,6 +3,7 @@ library("dplyr")
 library("devtools")
 load_all("../seqUtils/")
 library("Rsamtools")
+library("purrr")
 
 #Import data
 atac_list = readRDS("../macrophage-chromatin/results/ATAC/ATAC_combined_accessibility_data.rds")
@@ -23,6 +24,28 @@ min_pvalue_list = list(naive = naive_eigen_pvalue,
                        IFNg_SL1344 = ifng_sl1344_eigen_pvalue)
 saveRDS(min_pvalue_list, "results/ATAC/QTLs/rasqual_min_pvalues.rds")
 
+#Extract the number of tests performed by eigenMT for each peak
+n_tests = map(min_pvalue_list, ~dplyr::select(.,gene_id, n_tests)) %>% reduce(rbind) %>% unique()
+
+#Find minimal p-values from fastQTL results
+naive_fqtl = importFastQTLTable("results/ATAC/fastqtl/output/naive_50kb_cqn_perm.txt.gz") %>% 
+  fastQTLCorrectEigenMT(n_tests)
+ifng_fqtl = importFastQTLTable("results/ATAC/fastqtl/output/IFNg_50kb_cqn_perm.txt.gz") %>% 
+  fastQTLCorrectEigenMT(n_tests)
+sl1344_fqtl = importFastQTLTable("results/ATAC/fastqtl/output/SL1344_50kb_cqn_perm.txt.gz") %>% 
+  fastQTLCorrectEigenMT(n_tests)
+ifng_sl1344_fqtl = importFastQTLTable("results/ATAC/fastqtl/output/IFNg_SL1344_50kb_cqn_perm.txt.gz") %>% 
+  fastQTLCorrectEigenMT(n_tests)
+
+fastqtl_pvalue_list = list(naive = naive_fqtl,
+                           IFNg = ifng_fqtl,
+                           SL1344 = sl1344_fqtl, 
+                           IFNg_SL1344 = ifng_sl1344_fqtl)
+saveRDS(fastqtl_pvalue_list, "results/ATAC/QTLs/fastqtl_min_pvalues.rds")
+
+#Calculate Pi1
+pi1_stat = calculatePairwisePi1(fastqtl_pvalue_list)
+write.table(pi1_stat, "results/ATAC/QTLs/fastqtl_pi1_results.txt", sep = "\t", quote = FALSE)
 
 #Make database connections
 naive_sql <- src_sqlite("~/projects/macrophage-chromatin/naive_100kb.sqlite3") %>% tbl("rasqual")
