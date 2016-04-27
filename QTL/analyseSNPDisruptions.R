ed154ecc7a67c6d9c0e1c4a34c8cfd4070cd31bd
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-04-27 19:27:26 +0300

    Perform SNP disruption analysis only with the enriched motifs

--- QTL/analyseSNPDisruptions.R
+++ QTL/analyseSNPDisruptions.R
@@ -28,25 +28,25 @@ mean_tpm = calculateMean(combined_expression_data$tpm, as.data.frame(combined_ex
 expressed_genes = names(which(apply(mean_tpm, 1, max) > 1))
 expressed_motifs = dplyr::filter(unique_motifs, gene_id %in% expressed_genes)
 
+#Import motifs enriched in macrophages
+mf_enriched_motifs = read.table("results/ATAC/motif_analysis/cisBP_selected_enriched_motifs.txt", header = TRUE)
+            
 #Expressed_motifs
 cisbp_pwm_expressed = cisbp_pwm_list[intersect(names(cisbp_pwm_list), expressed_motifs$motif_id)]
+cisbp_pwm_enriched = cisbp_pwm_list[mf_enriched_motifs$motif_id]
 
 #Import ATAC peak sequences from disk
 sequences = readDNAStringSet("annotations/ATAC_consensus_peaks.fasta")
 peak_ids = strsplit(names(sequences), "=") %>% lapply(function(x) x[2]) %>% unlist()
 names(sequences) = peak_ids
 
-res = quantifyMotifDisruption(cisbp_pwm_list[["M6484_1.02"]], "ATAC_peak_145162", "rs7594476", atac_data$gene_metadata, sequences, snp_info) %>%
-  dplyr::filter(abs(rel_diff) > 0, max_rel_score > 0.8)
-
-
 #Find all possible disruptions
-motif_disruptions = purrr::map(as.list(cisbp_pwm_expressed), ~quantifyMotifDisruption(., "ATAC_peak_145162", "rs7594476", atac_data$gene_metadata, sequences, snp_info) %>%
+motif_disruptions = purrr::map(as.list(cisbp_pwm_enriched), ~quantifyMotifDisruption(., "ATAC_peak_145162", "rs7594476", atac_data$gene_metadata, sequences, snp_info) %>%
                                  dplyr::filter(abs(rel_diff) > 0, max_rel_score > 0.8))
+disruption_table = purrr::map_df(motif_disruptions, identity) %>% 
+  dplyr::left_join(expressed_motifs, by = "motif_id")
 
-dispruption_table = plyr::ldply(motif_disruptions)
-
-filtered = dplyr::left_join(dispruption_table, expressed_motifs) %>% 
+filtered = dplyr::left_join(disruption_table, expressed_motifs) %>% 
   dplyr::arrange(-abs(rel_diff)) %>%
   dplyr::group_by(tf_name) %>% 
   dplyr::filter(row_number() == 1)
