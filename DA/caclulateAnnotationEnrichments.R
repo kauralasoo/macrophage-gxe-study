bc89e20e6c3b5a4662df0906357add5abdac4c7b
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-05-01 19:27:22 +0300

    Caclulate ATAC enrichments for PU1 and CEBPb peaks

--- DA/caclulateAnnotationEnrichments.R
+++ DA/caclulateAnnotationEnrichments.R
@@ -31,6 +31,10 @@ gat-run.py --segments=results/ATAC/DA/ATAC_clustered_peaks.bed --annotations=res
 gat-run.py --segments=results/ATAC/DA/ATAC_clustered_peaks.bed --annotations=results/Ivashkiv/peak_calls/IRF1_joint_peaks.bed --workspace=../../annotations/blacklists/GRCh38_filtered_gapped_genome.bed --num-samples=1000 --log=results/annotation_overlaps/IRF1_overlap.gat_log.txt --with-segment-tracks > results/annotation_overlaps/IRF1_overlap.gat.txt
 gat-run.py --segments=results/ATAC/DA/ATAC_clustered_peaks.bed --annotations=results/Knight/CIITA-RFX5_joint_peaks.bed --workspace=../../annotations/blacklists/GRCh38_filtered_gapped_genome.bed --num-samples=1000 --log=results/annotation_overlaps/CIITA-RFX5_overlap.gat_log.txt --with-segment-tracks > results/annotation_overlaps/CIITA-RFX5_overlap.gat.txt
 
+#Enrichments for PU.1, CEBPb and CTCF
+gat-run.py --segments=annotations/ATAC_consensus_peaks.bed  --annotations=results/ATAC/ChIP_enrichment/naive_combined_peaks.bed --workspace=../../annotations/blacklists/GRCh38_filtered_gapped_genome.bed --num-samples=100 --log=results/ATAC/ChIP_enrichment/gat_output/ATAC_naive_overlap.gat_log.txt --with-segment-tracks > results/ATAC/ChIP_enrichment/gat_output/ATAC_naive_overlap.gat.txt
+gat-run.py --segments=results/ATAC/ChIP_enrichment/naive_combined_peaks.bed  --annotations=results/ATAC/ChIP_enrichment/naive_combined_peaks.bed --workspace=../../annotations/blacklists/GRCh38_filtered_gapped_genome.bed --num-samples=100 --log=results/ATAC/ChIP_enrichment/gat_output/naive_naive_overlap.gat_log.txt --with-segment-tracks > results/ATAC/ChIP_enrichment/gat_output/naive_naive_overlap.gat.txt
+
 
 #CIITA-RFX5
 ciita_enrich = read.table("results/annotation_overlaps/CIITA-RFX5_overlap.gat.txt", header = TRUE, stringsAsFactors = FALSE) %>%
@@ -39,4 +43,9 @@ ciita_enrich$track = factor(ciita_enrich$track,
       levels = rev(c("SL1344_up_1","SL1344_up_2" ,"IFNg_SL1344_up", "IFNg_up_1", "IFNg_up_2", "IFNg_down", "SL1344_down")))
 ciita_enrich_plot = plotEnrichment(ciita_enrich, xlabel = "Monocyte ChIP-Seq", ylabel = "ATAC-Seq peaks")
 ggsave("results/ATAC/DA/CIITA-RFX5_concordance.pdf", ciita_enrich_plot, width = 7, height = 7)
+
+#PU.1 and CEBPb from two different papers
+naive_enrich = read.table("results/ATAC/ChIP_enrichment/gat_output/naive_naive_overlap.gat.txt", header = TRUE, stringsAsFactors = FALSE) %>%
+  dplyr::select(track, annotation, l2fold)
+plotEnrichment(naive_enrich, xlabel = "Monocyte ChIP-Seq", ylabel = "ATAC-Seq peaks")
 
--- munge/importChipPeaks.R
+++ munge/importChipPeaks.R
@@ -1,13 +1,37 @@
 library("devtools")
+library("dplyr")
 load_all("../seqUtils/")
 
-processed/Rehli/MAC_PU1/MAC_PU1_peaks.narrowPeak
-
+#### Import peaks for enrichment in naive condition ####
 #Load Rehli dataset
 sample_names = c("MAC_PU1","MAC_CEBPbeta","CTCF_MAC")
-rehli_peaks = loadNarrowPeaks("processed/Rehli/", sample_names)
+rehli_peaks = loadNarrowPeaks("results/Rehli/peak_calls/", sample_names, sub_dir = FALSE)
+elementMetadata(rehli_peaks$MAC_PU1)$name = "PU.1_Pham"
+elementMetadata(rehli_peaks$MAC_CEBPbeta)$name = "CEBPb_Pham"
+elementMetadata(rehli_peaks$CTCF_MAC)$name = "CTCF_Pham"
+rehli_peaks = purrr::map(rehli_peaks, function(x){elementMetadata(x) = elementMetadata(x)[,"name",drop=FALSE]; return(x)})
 
-#Load Ivashkiv peaks
+#Load OCallaghan datset
+cebpb_ctrl_names = c("CEBPbeta_ctrl_201","CEBPbeta_ctrl_202","CEBPbeta_ctrl_203","CEBPbeta_ctrl_204")
+CEBPbeta_ctrl_peaks = loadNarrowPeaks("results/OCallaghan/peak_calls/", cebpb_ctrl_names, sub_dir = FALSE) %>% 
+  filterOverlaps(minOverlapCount = 2) %>% listUnion()
+elementMetadata(CEBPbeta_ctrl_peaks)$name = "CEBPb_Reschen"
+
+#Load Schultze dataset
+schultze_names = c("PU1_naive")
+schultze_peaks = loadNarrowPeaks("results/Schultze/peak_calls/", schultze_names, sub_dir = FALSE)
+PU1_Schultze = schultze_peaks$PU1_naive
+PU1_Schultze = PU1_Schultze[PU1_Schultze$score > 100,]
+elementMetadata(PU1_Schultze)$name = "PU.1_Schmidt"
+elementMetadata(PU1_Schultze) = elementMetadata(PU1_Schultze)[,"name", drop=FALSE]
+
+#Join peaks together
+joint_peaks = c(rehli_peaks$MAC_PU1, rehli_peaks$MAC_CEBPbeta, rehli_peaks$CTCF_MAC, CEBPbeta_ctrl_peaks, PU1_Schultze)
+rtracklayer::export.bed(joint_peaks, "results/ATAC/ChIP_enrichment/naive_combined_peaks.bed")
+saveRDS(joint_peaks, "results/ATAC/ChIP_enrichment/naive_combined_peaks.rds")
+
+
+#### Load Ivashkiv peaks ####
 sample_names = c("STAT1_rep1_A","STAT1_rep1_B","STAT1_rep1_C","STAT1_rep1_D","STAT1_rep2_B","STAT1_rep2_D",
                  "IRF1_A","IRF1_B","IRF1_E","IRF1_F")
 ivashkiv_peaks = loadNarrowPeaks("processed/Ivashkiv/", sample_names)
