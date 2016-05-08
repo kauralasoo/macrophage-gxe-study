e0973365815be03bd3ac4338d7845dc564b4809c
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-05-08 17:20:55 +0100

    Identify motif disruption events for all unique peaks

--- finemapping/caQTL_motif_disruptions.R
+++ finemapping/caQTL_motif_disruptions.R
@@ -1,13 +1,12 @@
+#Scan all caQTLs for motif disruptions
+library("devtools")
+library("dplyr")
+library("purrr")
 load_all("../seqUtils/")
+library("TFBSTools")
 
 #Import ATAC data
 atac_data = readRDS("results/ATAC/ATAC_combined_accessibility_data_covariates.rds")
-
-#Import condition-specific ATAC QTLs from disk
-variable_qtls = readRDS("results/ATAC/QTLs/rasqual_appear_disappear_qtls.rds")
-
-#Import putative causal variants
-unique_peaks = readRDS("results/ATAC/QTLs/unique_qtl_peaks.rds")
 
 #Import cisbp motifs
 cisbp_pwm_list = readRDS("results/ATAC/cisBP_PWMatrixList.rds")
@@ -23,42 +22,30 @@ mf_enriched_motifs = read.table("results/ATAC/motif_analysis/cisBP_selected_enri
 motif_names = dplyr::select(mf_enriched_motifs, motif_id, tf_name)
 cisbp_pwm_enriched = cisbp_pwm_list[mf_enriched_motifs$motif_id]
 
+#Import putative causal variants
+unique_peaks = readRDS("results/ATAC/QTLs/unique_qtl_peaks.rds")
+unique_peaks_filtered = dplyr::filter(unique_peaks$peak_snp_pairs, snp_count <= 3)
+
 #Import ATAC peak sequences from disk
 sequences = Biostrings::readDNAStringSet("annotations/ATAC_consensus_peaks.fasta")
 peak_ids = strsplit(names(sequences), "=") %>% lapply(function(x) x[2]) %>% unlist()
 names(sequences) = peak_ids
 
-#Extract IFNg-specific caQTLs
-qtls = dplyr::filter(variable_qtls$appear, cluster_id == 3) %>% dplyr::select(gene_id, snp_id) %>% unique()
-
-#Identify potential causal snps
-cset_filtered = dplyr::filter(unique_peaks$lead_credible_sets, gene_id %in% qtls$gene_id)
-overlap_genes = dplyr::semi_join(cset_filtered, qtls, by = c("gene_id","snp_id"))
-credible_snps = dplyr::semi_join(unique_peaks$peak_snp_pairs, overlap_genes, by = "gene_id") %>%
-  dplyr::filter(snp_count <= 2)
-
-credible_snps_no_indel = dplyr::semi_join(credible_snps, dplyr::filter(snp_info, snp_id %in% credible_snps$snp_id) %>% 
-                                            dplyr::filter(is_indel == FALSE), by = "snp_id")
-
 #Calculate motif disruptions
-motif_disruptions = quantifyMultipleVariants(credible_snps_no_indel, cisbp_pwm_enriched, atac_data$gene_metadata, sequences, snp_info)
+motif_disruptions = quantifyMultipleVariants(unique_peaks_filtered[1:10,], cisbp_pwm_enriched, atac_data$gene_metadata, sequences, snp_info)
+saveRDS(motif_disruptions, "results/ATAC/motif_analysis/unique_peaks_disrupted_motifs.rds")
 
 #Filter the results
-disruption_table_2 = dplyr::left_join(motif_disruptions, motif_names, by = "motif_id") %>%
-  dplyr::group_by(gene_id, snp_id, motif_id) %>% 
-  dplyr::arrange(-abs(rel_diff)) %>%
-  dplyr::filter(row_number() == 1) %>%
-  dplyr::ungroup() %>%
-  dplyr::arrange(-abs(rel_diff)) %>%
-  dplyr::filter(rel_diff > 0.05)
+#disruption_table_3 = dplyr::left_join(motif_disruptions, motif_names, by = "motif_id") %>%
+#  dplyr::group_by(gene_id, snp_id, motif_id) %>% 
+#  dplyr::arrange(-abs(rel_diff)) %>%
+#  dplyr::filter(row_number() == 1) %>%
+#  dplyr::ungroup() %>%
+#  dplyr::arrange(-abs(rel_diff)) %>%
+#  dplyr::filter(rel_diff > 0.05)
 
 
-#Indels
-cs = dplyr::filter(credible_snps, gene_id == "ATAC_peak_256699")
-motif_disruptions = quantifyMultipleVariants(cs, cisbp_pwm_enriched, atac_data$gene_metadata, sequences, snp_info)
-
-d = quantifyMotifDisruption(cisbp_pwm_enriched[[1]], "ATAC_peak_256699", "rs75758825", atac_data$gene_metadata, sequences, snp_info)
 
 
-a = quantifyMultipleMotifs("ATAC_peak_110001","rs11080327",cisbp_pwm_enriched, atac_data$gene_metadata, sequences, snp_info)
+
 
