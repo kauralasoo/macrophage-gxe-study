9413e25a92a60ea9a5d854d670f73c9f94aaa2d6
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-05-16 17:39:07 +0100

    Test for gene-gene-condition interactions

--- SL1344/finemapping/indentifySharedEQTLs.R
+++ SL1344/finemapping/indentifySharedEQTLs.R
@@ -7,6 +7,13 @@ credible_sets = readRDS("results/SL1344/eQTLs/rasqual_credible_sets.rds")
 #Import the VCF file
 vcf_file = readRDS("../macrophage-gxe-study/genotypes/SL1344/imputed_20151005/imputed.86_samples.sorted.filtered.named.rds")
 
+#Import RNA-Seq data
+rna_list = readRDS("results/SL1344/combined_expression_data_covariates.rds")
+min_pvalues_list = readRDS("results/SL1344/eQTLs/rasqual_min_pvalues.rds")
+min_pvalues_hits = lapply(min_pvalues_list, function(x){dplyr::filter(x, p_fdr < 0.1)})
+min_pvalue_df = plyr::ldply(min_pvalues_hits, .id = "condition_name") %>% 
+  dplyr::group_by(gene_id) %>% dplyr::arrange(p_nominal)
+
 #Create a df of SNP positions
 snp_pos_df = vcf_file$snpspos %>% 
   dplyr::transmute(seqnames = chr, start = pos, end = pos, strand = "+", snp_id = snpid)
@@ -40,18 +47,48 @@ pairwise_shared = dplyr::bind_cols(queries, subjects) %>%
   dplyr::select(master_id, master_condition, dependent_id, dependent_condition) %>% 
   unique()
 
-a = credible_sets$naive$ENSG00000164308$snp_id
-b = credible_sets$IFNg$ENSG00000113441$snp_id
-length(intersect(a,b))/length(union(a,b))
+#Calculate Jacccard scores for the credible sets
+pairwise_shared_jaccard = purrr::by_row(pairwise_shared, credibleSetJaccard, credible_sets, .collate = "rows")
+gene_pairs = dplyr::filter(pairwise_shared_jaccard, jaccard > 0.5) %>% 
+  dplyr::group_by(master_id, dependent_id) %>%
+  dplyr::arrange(p_nominal) %>%
+  dplyr::filter(row_number() == 1) %>%
+  dplyr::group_by(master_id, dependent_id,snp_id) %>%
+  dplyr::summarise(jaccard = max(jaccard)) %>%
+  dplyr::ungroup()
 
 #Convert shared peaks into clusters
-clusters = constructClustersFromGenePairs(pairwise_shared)
+clusters = constructClustersFromGenePairs(dplyr::select(gene_pairs, master_id, dependent_id))
 
 #Use Coloc to test if the QTLs are indeed shared
 
 #Test for GxGxE interactions
+#Extract data
+naive_ifng_rna = extractConditionFromExpressionList(c("naive","IFNg"), rna_list)
+naive_sl1344_rna = extractConditionFromExpressionList(c("naive","SL1344"), rna_list)
+naive_ifng_sl1344_rna = extractConditionFromExpressionList(c("naive","IFNg_SL1344"), rna_list)
 
+#Specify models of interest
+covariate_names = c("sex_binary", "ng_ul_mean","macrophage_diff_days","rna_auto", "max_purity_filtered", "harvest_stimulation_days",
+                    "PEER_factor_1", "PEER_factor_2", "PEER_factor_3","PEER_factor_4", "PEER_factor_5","PEER_factor_6")
 
+model0 = as.formula(paste("cqn ~ genotype + peak_type + condition_name + peak_type*condition_name + 
+                    genotype*peak_type + genotype*condition_name ", 
+                               paste(covariate_names, collapse = " + "), sep = "+ "))
+model1 = as.formula(paste("cqn ~genotype + peak_type + condition_name + peak_type*condition_name + 
+                    genotype*peak_type + genotype*condition_name + genotype*condition_name*peak_type ", 
+                                       paste(covariate_names, collapse = " + "), sep = "+ "))
 
+#Test for interactions
+peak_peak_interactions = purrr::by_row(gene_pairs, testThreewayInteraction, naive_ifng_rna$cqn, 
+                                       naive_ifng_rna$sample_metadata, vcf_file, model0, model1, .collate = "rows", .to = "naive_vs_IFNg_pvalue")
+naive_sl1344_ineractions = purrr::by_row(gene_pairs, testThreewayInteraction, naive_ifng_rna$cqn, 
+                                       naive_ifng_rna$sample_metadata, vcf_file, model0, model1, .collate = "rows", .to = "naive_vs_SL1344_pvalue")
+naive_ifng_sl1344_ineractions = purrr::by_row(gene_pairs, testThreewayInteraction, naive_ifng_rna$cqn, 
+                                       naive_ifng_rna$sample_metadata, vcf_file, model0, model1, .collate = "rows", .to = "naive_vs_IFNg_SL1344_pvalue")
+
+#Visualise a couple of examples
+res = testThreewayInteraction(a[16,], naive_ifng_rna$cqn, naive_ifng_rna$sample_metadata, vcf_file, model0, model1, p_only = FALSE)
+ggplot(res$data, aes(x = factor(genotype), y = cqn)) + geom_point() + geom_boxplot() + facet_grid(peak_type ~ condition_name)
 
 
