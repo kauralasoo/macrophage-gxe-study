8219d730444aa4510b35559c2b1ed81f7e0988d8
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-05-19 17:20:40 +0100

    Revise identifying master peaks and make some summary plots

--- finemapping/identifyMasterPeaks.R
+++ finemapping/identifyMasterPeaks.R
@@ -81,7 +81,7 @@ shared_snp_ids = c(shared_snps$master_snp_id, shared_snps$overlap_snp_id) %>% un
 
 #Remove additional LD friends
 unique_lead_snps_filtered = dplyr::filter(unique_lead_snps, !(snp_id %in% shared_snp_ids))
-unique_credible_sets = dplyr::semi_join(credible_sets_by_condition, unique_lead_snps, by = c("gene_id", "condition_name"))
+unique_credible_sets = dplyr::semi_join(credible_sets_by_condition, unique_lead_snps_filtered, by = c("gene_id", "condition_name"))
 unique_masters_counted = dplyr::filter(unique_masters_counted, gene_id %in% unique_credible_sets$gene_id)
 
 #Combine unique results
@@ -99,16 +99,10 @@ peaks_with_shared_lead_snps = dplyr::left_join(shared_snps, peak_snp_map, by = c
   dplyr::rename(overlap_peak_id = gene_id) %>% 
   dplyr::select(master_peak_id, overlap_peak_id)
 shared_masters = rbind(shared_masters, peaks_with_shared_lead_snps) %>% unique()
+shared_lead_peaks = unique(c(peaks_with_shared_lead_snps$master_peak_id, peaks_with_shared_lead_snps$overlap_peak_id))
 
 #Convert shared peaks into clusters
-graph = igraph::graph_from_data_frame(shared_masters, directed = FALSE)
-clusters = igraph::clusters(graph)
-atac_clusters = data_frame(gene_id = names(clusters$membership), cluster_number = clusters$membership) %>% 
-  arrange(cluster_number) %>% 
-  dplyr::mutate(cluster_id = paste0("ATAC_cluster_",cluster_number)) %>% 
-  dplyr::select(gene_id, cluster_id)
-peak_counts = dplyr::group_by(atac_clusters, cluster_id) %>% dplyr::summarise(peak_count = length(gene_id))
-atac_clusters = dplyr::left_join(atac_clusters, peak_counts, by = "cluster_id")
+atac_clusters = constructClustersFromGenePairs(shared_masters, cluster_name_prefix = "ATAC_cluster_")
 
 #Count the number of SNPs in each peak and cluster
 atac_clusters_counted = dplyr::left_join(atac_clusters, potential_master_peaks, by = "gene_id") %>% 
@@ -130,19 +124,19 @@ cluster_lead_snps = dplyr::left_join(atac_clusters_counted, potential_master_pea
 cluster_peaks = list(cluster_memberships = atac_clusters_counted, lead_snps = cluster_lead_snps)
 saveRDS(cluster_peaks, "results/ATAC/QTLs/clustered_qtl_peaks.rds")
 
-#Explore peaks that do not overlap their own credible sets
+### Dependent peaks with significant master QTLs ####
+#Find all caQTLs whose credible set overlaps some other peak, but not the peak itself
 no_master_qtls = dplyr::filter(shared_credible_sets, master_peak_id != overlap_peak_id) %>% 
   dplyr::anti_join(master_peak_list, by = c("master_peak_id" = "gene_id"))
-no_master_peak_count = unique(no_master_qtls$master_peak_id) %>% length()
-unique_olaps = dplyr::semi_join(no_master_qtls, unique_lead_snps,by = c("overlap_peak_id" = "gene_id")) %>% dplyr::select(master_peak_id) %>% unique()
-cluster_olaps = dplyr::semi_join(no_master_qtls, cluster_lead_snps,by = c("overlap_peak_id" = "gene_id")) %>% dplyr::select(master_peak_id) %>% unique()
 
-#Find the master peak and its QTL
-master_unique = dplyr::semi_join(no_master_qtls, unique_lead_snps,by = c("overlap_peak_id" = "gene_id"))
-dependent_uniq_masters = dplyr::left_join(master_unique, unique_lead_snps, by = c("overlap_peak_id" = "gene_id")) %>% 
+#Find all of the cases where the other peak is a unique master caQTL peak
+dependent_uniq_masters = dplyr::semi_join(no_master_qtls, unique_lead_snps_filtered,by = c("overlap_peak_id" = "gene_id")) %>%
+  dplyr::left_join(unique_lead_snps_filtered, by = c("overlap_peak_id" = "gene_id")) %>% 
   dplyr::rename(dependent_id = master_peak_id, master_id = overlap_peak_id)
-master_cluster = dplyr::semi_join(no_master_qtls, cluster_lead_snps,by = c("overlap_peak_id" = "gene_id"))
-dependent_cluster_masters = dplyr::left_join(master_cluster, cluster_lead_snps, by = c("overlap_peak_id" = "gene_id")) %>% 
+
+#Find all of the cases where the other peak is belongs to a cluster of master peaks
+dependent_cluster_masters = dplyr::semi_join(no_master_qtls, cluster_lead_snps,by = c("overlap_peak_id" = "gene_id")) %>%
+  dplyr::left_join(cluster_lead_snps, by = c("overlap_peak_id" = "gene_id")) %>% 
   dplyr::rename(dependent_id = master_peak_id, master_id = overlap_peak_id)
 res = list(unique_masters = dependent_uniq_masters, cluster_master = dependent_cluster_masters)
 saveRDS(res, "results/ATAC/QTLs/depdendent_peaks.rds")
@@ -155,10 +149,69 @@ overlap_peak_count = purrr::map(credible_sets_df, ~dplyr::filter(.,!is.na(overla
 overlap_same_peak = dplyr::select(potential_master_peaks, gene_id) %>% unique() %>% nrow()
 unique_master_peaks = length(unique_masters_counted$gene_id %>% unique)
 shared_master_peaks = nrow(atac_clusters_counted)
+other_is_qtl = length(unique(master_cluster$master_peak_id)) + length(unique(master_unique$master_peak_id))
 
 #Compile stats
-summary_stats = data_frame(type = c("total_count", "overlaps_any_peak", "overlaps_same_peak", "shared","unique"),
-                           peak_count = c(total_peak_count,overlap_peak_count,overlap_same_peak,shared_master_peaks, unique_master_peaks))
+summary_stats = data_frame("total" = total_peak_count, "overlap_any_peak" = overlap_peak_count, "overlap_same_peak" = overlap_same_peak, 
+                           "shared_masters" = shared_master_peaks, "unique_masters" = unique_master_peaks, "other_is_qtl" = other_is_qtl) %>%
+  dplyr::mutate(overlap_other_peak = overlap_any_peak-overlap_same_peak, overlap_no_peak = total - overlap_any_peak) %>%
+  dplyr::mutate(other_not_qtl = overlap_other_peak - other_is_qtl)
 write.table(summary_stats, "results/ATAC/QTLs/properties/credible_set_peak_overlaps.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+
+#Make a plot of counts
+summary_df = dplyr::select(summary_stats, unique_masters, shared_masters, other_is_qtl, other_not_qtl, overlap_no_peak) %>% 
+  tidyr::gather("type", "count")
+plot = ggplot(summary_df, aes(x = factor(1), fill = type, y = count)) + 
+  geom_bar(stat = "identity", position = "stack") +
+  scale_fill_brewer(palette="Set1") +
+  theme(legend.position="none")
+ggsave("results/ATAC/QTLs/properties/caQTL_classification.pdf", plot = plot, width = 1.4, height = 4)
+
+
+#Plot the number of peaks per cluster
+cluster_size_df = dplyr::transmute(atac_clusters_counted, cluster_id, peak_count = factor(peak_count)) %>% unique()
+count_plot = ggplot(cluster_size_df, aes(x = peak_count)) + geom_bar() + theme_hc() +
+  xlab("Number of peaks")
+ggsave("results/ATAC/QTLs/properties/number_of_peaks_per_cluster.pdf", plot = count_plot, width = 4, height = 4)
+
+#Plot the number of SNPs per unique QTL peak
+snp_count_df = dplyr::select(unique_masters_counted, gene_id, snp_count) %>% unique() %>%
+  dplyr::filter(snp_count <= 10) %>%
+  dplyr::mutate(snp_count = factor(snp_count))
+snp_count_plot = ggplot(snp_count_df, aes(x = snp_count)) + geom_bar() + theme_hc() +
+  xlab("Number of variants")
+ggsave("results/ATAC/QTLs/properties/number_of_variants_per_unqiue_master.pdf", plot = snp_count_plot, width = 4, height = 4)
+
+#Count the number of dependent peaks per master
+master_dependent_pairs = rbind(dplyr::select(dependent_uniq_masters, dependent_id, master_id), dplyr::select(dependent_cluster_masters, dependent_id, master_id))
+dependent_peak_count = master_dependent_pairs %>% 
+  dplyr::group_by(master_id) %>% 
+  dplyr::summarise(dependent_peak_count = length(dependent_id)) %>% 
+  arrange(-dependent_peak_count)
+dependent_plot = ggplot(dependent_peak_count, aes(x = dependent_peak_count)) + geom_bar() + theme_hc() +
+  xlab("Number of dependent peaks")
+ggsave("results/ATAC/QTLs/properties/number_of_dependent_peaks_per_unqiue_master.pdf", plot = dependent_plot, width = 4, height = 4)
+
+#Plot distances between master and dependent peaks
+dependent_distances = calculatePeakDistance(master_dependent_pairs, atac_data$gene_metadata)
+dependent_distance_plot = ggplot(dependent_distances, aes(x = distance/1000)) + geom_histogram(binwidth = 2) + theme_hc() +
+  xlab("Distance from master peak")
+ggsave("results/ATAC/QTLs/properties/master_dependent_peak_distance.pdf", plot = dependent_distance_plot, width = 4, height = 4)
+
+#Plot the number of variants per cluster
+cluster_variants = ggplot(cluster_lead_snps, aes(x = cluster_snp_count)) + 
+  geom_bar() + 
+  scale_x_continuous(limits = c(0,50)) + 
+  theme_hc() + 
+  xlab("Number of variants per cluster")
+ggsave("results/ATAC/QTLs/properties/number_of_variants_per_cluster.pdf", plot = cluster_variants, width = 4, height = 4)
+
+#Calculate distances within clusters
+cluster_dist = dplyr::rename(shared_masters, master_id = master_peak_id, dependent_id = overlap_peak_id) %>% 
+  calculatePeakDistance(atac_list$gene_metadata)
+cluster_dist_plot = ggplot(cluster_dist, aes(x = distance/1000)) + geom_histogram(binwidth = 2) + theme_hc() +
+  xlab("Distance between two peaks in cluster") + 
+  scale_x_continuous(limits = c(-50, 50))
+ggsave("results/ATAC/QTLs/properties/within_cluster_distances.pdf", plot = cluster_dist_plot, width = 4, height = 4)
 
 
