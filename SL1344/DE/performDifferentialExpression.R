bdc20029a9f6cc970f022e20e1ca07003e6170be
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-05-24 15:58:09 +0100

    Remove old differential expression code.

--- SL1344/DE/performDifferentialExpression.R
+++ SL1344/DE/performDifferentialExpression.R
@@ -10,7 +10,6 @@ load_all("../seqUtils/")
 library("pheatmap")
 
 #Load expression data from disk
-#expression_list = readRDS("results/SL1344/combined_expression_data.rds")
 eqtl_data_list = readRDS("results/SL1344/combined_expression_data_covariates.rds")
 
 #Make heatmap
@@ -33,42 +32,7 @@ pca_plot = ggplot(pca_list$pca_matrix, aes(x = PC1, y = PC2, color = condition_n
   scale_color_discrete(name = "condition")
 ggsave("results/SL1344/DE/PCA_of_gene_expression.pdf", plot = pca_plot, width = 5.5, height = 4)
 
-#Perform DEseq (This takes a really long time!)
-dds_gene_meta = dplyr::select(gene_metadata,gene_id, gene_name, gene_biotype)
-dds = DESeqDataSetFromMatrix(counts, design_matrix, ~SL1344 + IFNg)
-saveRDS(dds, "results/SL1344/DESeq2_results_full_data.rds")
-dds = readRDS("results/SL1344/DESeq2_results_full_data.rds")
-
-#IFNg results
-ifng_results = results(dds, contrast = c("IFNg","primed","naive"))
-ifng_results_filtered = filterDESeqResults(ifng_results, dds_gene_meta, biotype_filter = "protein_coding")
-
-#Salmonella results
-sl1344_results = results(dds, contrast = c("SL1344","infected","control"))
-sl1344_results_filtered = filterDESeqResults(sl1344_results, dds_gene_meta, biotype_filter = "protein_coding")
-
-#Interaction_genes
-interaction_results = results(dds, name = "SL1344infected.IFNgprimed")
-interaction_results_filtered = filterDESeqResults(interaction_results, dds_gene_meta, biotype_filter = "protein_coding")
-
-#Iddentify syneregestically activated genes
-pos_interaction = dplyr::semi_join(exprs_cqn_mean, interaction_results_filtered$up_genes, by = "gene_id") %>% 
-  dplyr::left_join(dds_gene_meta, by = "gene_id") %>%
-  dplyr::mutate(salmonella_fc = C-A, interaction_fc = D-A)
-synergestic_activation = dplyr::filter(pos_interaction, interaction_fc - salmonella_fc > 1, interaction_fc > 1)
-
-
-#Perform GO analysis
-ifng_up_go = gprofiler(ifng_results_filtered$up_genes$gene_id, organism = "hsapiens") %>% tbl_df() %>% arrange(p.value)
-sl1344_up_go = gprofiler(sl1344_results_filtered$up_genes$gene_id, organism = "hsapiens") %>% tbl_df() %>% arrange(p.value)
-dplyr::filter(sl1344_up_go, term.size < 300) %>% dplyr::select(p.value, term.name, term.size) %>% View()
-
-go_results = list(ifng_up_go = ifng_up_go, sl1344_up_go = sl1344_up_go)
-saveRDS(go_results, "results/SL1344/diffExp/GO_results.rds")
-
 #Make plot of IFNb
-design_matrix = expression_list$design
-design_matrix$condition_name = factor(design_matrix$condition_name, levels = c("naive","IFNg","SL1344", "IFNg_SL1344"))
 ifnb_plot = plotGene("ENSG00000171855",expression_list$exprs_cqn, design_matrix, expression_list$gene_metadata)
 ggsave("results/SL1344/diffExp/IFNB1.pdf", plot = ifnb_plot, width = 5, height = 5)
 
