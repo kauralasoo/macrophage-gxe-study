f39f1808fed449271cb3ef29a92781e69bb1d2df
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-06-17 20:05:07 +0100

    Rerun fastQTL and tabix-index to output files

--- SL1344_run_FastQTL.sh
+++ SL1344_run_FastQTL.sh
@@ -22,26 +22,46 @@ rm results/SL1344/fastqtl/output/*.chunk_*
 
 
 #Get full p-values from fastQTL
-cat results/SL1344/fastqtl/input/chunk_table.txt | python ~/software/utils/submitJobs.py --MEM 3000 --jobname run_fastQTL --ncores 1 --command "python ~/software/utils/fastqtl/runFastQTL.py --vcf results/SL1344/fastqtl/input/fastqtl_genotypes.INFO_08.named.vcf.gz --bed results/SL1344/fastqtl/input/naive.expression.txt.gz --cov results/SL1344/fastqtl/input/naive.covariates.txt --W 500000 --out results/SL1344/fastqtl/output/naive_full --execute True"
-cat results/SL1344/fastqtl/input/chunk_table.txt | python ~/software/utils/submitJobs.py --MEM 3000 --jobname run_fastQTL --ncores 1 --command "python ~/software/utils/fastqtl/runFastQTL.py --vcf results/SL1344/fastqtl/input/fastqtl_genotypes.INFO_08.named.vcf.gz --bed results/SL1344/fastqtl/input/IFNg.expression.txt.gz --cov results/SL1344/fastqtl/input/IFNg.covariates.txt --W 500000 --out results/SL1344/fastqtl/output/IFNg_full --execute True"
-cat results/SL1344/fastqtl/input/chunk_table.txt | python ~/software/utils/submitJobs.py --MEM 3000 --jobname run_fastQTL --ncores 1 --command "python ~/software/utils/fastqtl/runFastQTL.py --vcf results/SL1344/fastqtl/input/fastqtl_genotypes.INFO_08.named.vcf.gz --bed results/SL1344/fastqtl/input/SL1344.expression.txt.gz --cov results/SL1344/fastqtl/input/SL1344.covariates.txt --W 500000 --out results/SL1344/fastqtl/output/SL1344_full --execute True"
-cat results/SL1344/fastqtl/input/chunk_table.txt | python ~/software/utils/submitJobs.py --MEM 3000 --jobname run_fastQTL --ncores 1 --command "python ~/software/utils/fastqtl/runFastQTL.py --vcf results/SL1344/fastqtl/input/fastqtl_genotypes.INFO_08.named.vcf.gz --bed results/SL1344/fastqtl/input/IFNg_SL1344.expression.txt.gz --cov results/SL1344/fastqtl/input/IFNg_SL1344.covariates.txt --W 500000 --out results/SL1344/fastqtl/output/IFNg_SL1344_full --execute True"
+cat results/SL1344/fastqtl/input/all_chunk_table.txt | python ~/software/utils/submitJobs.py --MEM 1000 --jobname run_fastQTL --ncores 1 --command "python ~/software/utils/fastqtl/runFastQTL.py --vcf results/SL1344/rasqual/input/naive.ASE.vcf.gz --bed results/SL1344/fastqtl/input/naive.expression_cqn.txt.gz --cov results/SL1344/fastqtl/input/naive.covariates.txt --W 500000 --out results/SL1344/fastqtl/output/naive_500kb_full --execute True"
+cat results/SL1344/fastqtl/input/all_chunk_table.txt | python ~/software/utils/submitJobs.py --MEM 1000 --jobname run_fastQTL --ncores 1 --command "python ~/software/utils/fastqtl/runFastQTL.py --vcf results/SL1344/rasqual/input/IFNg.ASE.vcf.gz --bed results/SL1344/fastqtl/input/IFNg.expression_cqn.txt.gz --cov results/SL1344/fastqtl/input/IFNg.covariates.txt --W 500000 --out results/SL1344/fastqtl/output/IFNg_500kb_full --execute True"
+cat results/SL1344/fastqtl/input/all_chunk_table.txt | python ~/software/utils/submitJobs.py --MEM 1000 --jobname run_fastQTL --ncores 1 --command "python ~/software/utils/fastqtl/runFastQTL.py --vcf results/SL1344/rasqual/input/SL1344.ASE.vcf.gz --bed results/SL1344/fastqtl/input/SL1344.expression_cqn.txt.gz --cov results/SL1344/fastqtl/input/SL1344.covariates.txt --W 500000 --out results/SL1344/fastqtl/output/SL1344_500kb_full --execute True"
+cat results/SL1344/fastqtl/input/all_chunk_table.txt | python ~/software/utils/submitJobs.py --MEM 1000 --jobname run_fastQTL --ncores 1 --command "python ~/software/utils/fastqtl/runFastQTL.py --vcf results/SL1344/rasqual/input/IFNg_SL1344.ASE.vcf.gz --bed results/SL1344/fastqtl/input/IFNg_SL1344.expression_cqn.txt.gz --cov results/SL1344/fastqtl/input/IFNg_SL1344.covariates.txt --W 500000 --out results/SL1344/fastqtl/output/IFNg_SL1344_500kb_full --execute True"
 
 
 #Merge chunks into single files
-zcat results/SL1344/fastqtl/output/naive_full.chunk_*.txt.gz | bgzip > results/SL1344/fastqtl/output/naive_pvalues.txt.gz
-zcat results/SL1344/fastqtl/output/IFNg_full.chunk_*.txt.gz | bgzip > results/SL1344/fastqtl/output/IFNg_pvalues.txt.gz
-zcat results/SL1344/fastqtl/output/SL1344_full.chunk_*.txt.gz | bgzip > results/SL1344/fastqtl/output/SL1344_pvalues.txt.gz
-zcat results/SL1344/fastqtl/output/IFNg_SL1344_full.chunk_*.txt.gz | bgzip > results/SL1344/fastqtl/output/IFNg_SL1344_pvalues.txt.gz
+zcat results/SL1344/fastqtl/output/naive_500kb_full.chunk_*.txt.gz | bgzip > results/SL1344/fastqtl/output/naive_500kb_pvalues.txt.gz
+zcat results/SL1344/fastqtl/output/IFNg_500kb_full.chunk_*.txt.gz | bgzip > results/SL1344/fastqtl/output/IFNg_500kb_pvalues.txt.gz
+zcat results/SL1344/fastqtl/output/SL1344_500kb_full.chunk_*.txt.gz | bgzip > results/SL1344/fastqtl/output/SL1344_500kb_pvalues.txt.gz
+zcat results/SL1344/fastqtl/output/IFNg_SL1344_500kb_full.chunk_*.txt.gz | bgzip > results/SL1344/fastqtl/output/IFNg_SL1344_500kb_pvalues.txt.gz
 
 #Remove chunks
 rm results/SL1344/fastqtl/output/*.chunk_*
 
 #Add SNP coordinates
-echo hello | python ~/software/utils/submitJobs.py --MEM 5000 --jobname fastQTL_add_coords --command "python ~/software/utils/fastqtl/fastqtlAddSnpCoordinates.py --vcf results/SL1344/fastqtl/input/fastqtl_genotypes.INFO_08.named.vcf.gz --fastqtl results/SL1344/fastqtl/output/naive_pvalues.txt.gz | bgzip > results/SL1344/fastqtl/output/naive_pvalues.coords.txt.gz"
-echo hello | python ~/software/utils/submitJobs.py --MEM 5000 --jobname fastQTL_add_coords --command "python ~/software/utils/fastqtl/fastqtlAddSnpCoordinates.py --vcf results/SL1344/fastqtl/input/fastqtl_genotypes.INFO_08.named.vcf.gz --fastqtl results/SL1344/fastqtl/output/IFNg_pvalues.txt.gz | bgzip > results/SL1344/fastqtl/output/IFNg_pvalues.coords.txt.gz"
-echo hello | python ~/software/utils/submitJobs.py --MEM 5000 --jobname fastQTL_add_coords --command "python ~/software/utils/fastqtl/fastqtlAddSnpCoordinates.py --vcf results/SL1344/fastqtl/input/fastqtl_genotypes.INFO_08.named.vcf.gz --fastqtl results/SL1344/fastqtl/output/SL1344_pvalues.txt.gz | bgzip > results/SL1344/fastqtl/output/SL1344_pvalues.coords.txt.gz"
-echo hello | python ~/software/utils/submitJobs.py --MEM 5000 --jobname fastQTL_add_coords --command "python ~/software/utils/fastqtl/fastqtlAddSnpCoordinates.py --vcf results/SL1344/fastqtl/input/fastqtl_genotypes.INFO_08.named.vcf.gz --fastqtl results/SL1344/fastqtl/output/IFNg_SL1344_pvalues.txt.gz | bgzip > results/SL1344/fastqtl/output/IFNg_SL1344_pvalues.coords.txt.gz"
+echo hello | python ~/software/utils/submitJobs.py --MEM 5000 --jobname fastQTL_add_coords --command "python ~/software/utils/fastqtl/fastqtlAddSnpCoordinates.py --vcf results/SL1344/rasqual/input/naive.ASE.vcf.gz --fastqtl results/SL1344/fastqtl/output/naive_500kb_pvalues.txt.gz | bgzip > results/SL1344/fastqtl/output/naive_500kb_pvalues.coords.txt.gz"
+echo hello | python ~/software/utils/submitJobs.py --MEM 5000 --jobname fastQTL_add_coords --command "python ~/software/utils/fastqtl/fastqtlAddSnpCoordinates.py --vcf results/SL1344/rasqual/input/IFNg.ASE.vcf.gz --fastqtl results/SL1344/fastqtl/output/IFNg_500kb_pvalues.txt.gz | bgzip > results/SL1344/fastqtl/output/IFNg_500kb_pvalues.coords.txt.gz"
+echo hello | python ~/software/utils/submitJobs.py --MEM 5000 --jobname fastQTL_add_coords --command "python ~/software/utils/fastqtl/fastqtlAddSnpCoordinates.py --vcf results/SL1344/rasqual/input/SL1344.ASE.vcf.gz --fastqtl results/SL1344/fastqtl/output/SL1344_500kb_pvalues.txt.gz | bgzip > results/SL1344/fastqtl/output/SL1344_500kb_pvalues.coords.txt.gz"
+echo hello | python ~/software/utils/submitJobs.py --MEM 5000 --jobname fastQTL_add_coords --command "python ~/software/utils/fastqtl/fastqtlAddSnpCoordinates.py --vcf results/SL1344/rasqual/input/IFNg_SL1344.ASE.vcf.gz --fastqtl results/SL1344/fastqtl/output/IFNg_SL1344_500kb_pvalues.txt.gz | bgzip > results/SL1344/fastqtl/output/IFNg_SL1344_500kb_pvalues.coords.txt.gz"
+
+#Sort files by SNP coordinates
+#awk command is necessary to change field separator from space to tab
+zcat results/SL1344/fastqtl/output/naive_500kb_pvalues.coords.txt.gz | awk -v OFS='\t' '{$1=$1; print $0}' | sort -k2,2 -k3,3n | bgzip > results/SL1344/fastqtl/output/naive_500kb_pvalues.sorted.txt.gz &
+zcat results/SL1344/fastqtl/output/IFNg_500kb_pvalues.coords.txt.gz | awk -v OFS='\t' '{$1=$1; print $0}' | sort -k2,2 -k3,3n | bgzip > results/SL1344/fastqtl/output/IFNg_500kb_pvalues.sorted.txt.gz &
+zcat results/SL1344/fastqtl/output/SL1344_500kb_pvalues.coords.txt.gz | awk -v OFS='\t' '{$1=$1; print $0}' | sort -k2,2 -k3,3n | bgzip > results/SL1344/fastqtl/output/SL1344_500kb_pvalues.sorted.txt.gz &
+zcat results/SL1344/fastqtl/output/IFNg_SL1344_500kb_pvalues.coords.txt.gz | awk -v OFS='\t' '{$1=$1; print $0}' | sort -k2,2 -k3,3n | bgzip > results/SL1344/fastqtl/output/IFNg_SL1344_500kb_pvalues.sorted.txt.gz &
+
+#Index the output files using Tabix
+tabix -s2 -b3 -e3 -f results/SL1344/fastqtl/output/naive_500kb_pvalues.sorted.txt.gz
+tabix -s2 -b3 -e3 -f results/SL1344/fastqtl/output/IFNg_500kb_pvalues.sorted.txt.gz
+tabix -s2 -b3 -e3 -f results/SL1344/fastqtl/output/SL1344_500kb_pvalues.sorted.txt.gz
+tabix -s2 -b3 -e3 -f results/SL1344/fastqtl/output/IFNg_SL1344_500kb_pvalues.sorted.txt.gz
+
+#Remove intermediate files
+rm results/SL1344/fastqtl/output/*pvalues.coords.txt.gz
+rm results/SL1344/fastqtl/output/*pvalues.txt.gz
+
+
+
 
 #Extract p-value for a subset of genes ENSG00000185245 ENSG00000079785 ENSG00000144228 ENSG00000173992
 echo "CHR BP SNP P\n" && zgrep ENSG00000109861 results/SL1344/fastqtl/output/IFNg_pvalues.coords.txt.gz | cut -f2,3,4,6 -d " " > results/SL1344/eQTLs/example_loci/CTSC/CTSC_pvalues.gwas
