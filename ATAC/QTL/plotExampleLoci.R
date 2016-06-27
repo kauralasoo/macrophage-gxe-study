e4bb992221869bb12c5769abe082619ca1d4231b
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-06-27 15:20:14 +0300

    Improve coverage plot

--- ATAC/QTL/plotExampleLoci.R
+++ ATAC/QTL/plotExampleLoci.R
@@ -14,9 +14,9 @@ vcf_file = readRDS("genotypes/SL1344/imputed_20151005/imputed.86_samples.sorted.
 
 #SPOPL enhancer
 region_coords = c(138677635, 138713158)
-selected_peaks = dplyr::filter(atac_list$gene_metadata, gene_id %in% 
+selected_peaks = dplyr::filter(atac_data$gene_metadata, gene_id %in% 
                                  c("ATAC_peak_154955", "ATAC_peak_154957", "ATAC_peak_154958"))
-selected_peaks = dplyr::filter(atac_list$gene_metadata, start > region_coords[1], end < region_coords[2], chr == 2)
+selected_peaks = dplyr::filter(atac_data$gene_metadata, start > region_coords[1], end < region_coords[2], chr == 2)
 peak_annot = wiggpleplotrConstructPeakAnnotations(selected_peaks)
 
 #Construct metadata df for wiggleplotr
@@ -26,14 +26,14 @@ track_data = wiggleplotrGenotypeColourGroup(meta_df, "rs12621644", vcf_file$geno
 #Filter by condtion
 filtered_tracks = dplyr::filter(track_data, track_id %in% c("naive", "IFNg"))
 
-#Make plot
+#Make a coverage plot of the ATAC data
 spopl_region = plotCoverage(exons = peak_annot$peak_list, cdss = peak_annot$peak_list, track_data = filtered_tracks, rescale_introns = FALSE, 
                 transcript_annotations = peak_annot$peak_annot, fill_palette = getGenotypePalette(), flanking_length = c(12000,300), 
                 connect_exons = FALSE, label_type = "peak", plot_fraction = 0.2, heights = c(0.7,0.3), 
                 region_coords = region_coords, return_subplots_list = TRUE)
 
 #Make a plot of the PU.1 ChIP-Seq data
-pu1_df = data_frame(sample_id = "PU1_naive", track_id = "PU.1", colour_group = "PU.1", scaling_factor = 1) %>%
+pu1_df = data_frame(sample_id = "PU1_naive", track_id = "PU.1", colour_group = "PU.1", scaling_factor = 10) %>%
   dplyr::mutate(bigWig = file.path("/Volumes/JetDrive/bigwigs/Schmidt", paste(sample_id, ".bw", sep = "")))
 spopl_region_pu1 = plotCoverage(exons = peak_annot$peak_list, cdss = peak_annot$peak_list, track_data = pu1_df, rescale_introns = FALSE, 
                             transcript_annotations = peak_annot$peak_annot, fill_palette = c("black"), flanking_length = c(300,300), 
@@ -41,14 +41,16 @@ spopl_region_pu1 = plotCoverage(exons = peak_annot$peak_list, cdss = peak_annot$
                             region_coords = region_coords, return_subplots_list = TRUE)
 
 #Make a plot of p-values from the region
-peak_pvalues = tabixFetchGenesQuick("ATAC_peak_145162",tabix_file = "results/ATAC/rasqual/output/naive_100kb/naive_100kb.sorted.txt.gz", 
-                                    gene_metadata = atac_list$gene_metadata, cis_window = 5e4)[[1]]
-peak_pvals = dplyr::select(peak_pvalues, pos, p_nominal)
-region_pvals = dplyr::filter(peak_pvals, pos > region_coords[1], pos < region_coords[2]) %>%
-  dplyr::mutate(track_id = "caQTL")
-associated_variants = makeManhattanPlot(region_pvals)
+naive_peak_pvalues = tabixFetchGenesQuick(c("ATAC_peak_145162"),
+                                    tabix_file = "results/ATAC/rasqual/output/naive_100kb/naive_100kb.sorted.txt.gz", 
+                                    gene_metadata = atac_data$gene_metadata, cis_window = 1e5)
+naive_peak_pvals = naive_peak_pvalues %>% 
+  purrr::map_df(., ~dplyr::mutate(., track_id = "naive")) %>% 
+  dplyr::select(gene_id, pos, p_nominal, track_id)
+
+associated_variants = makeManhattanPlot(naive_peak_pvals, region_coords)
   
 #Combine the two plots together
 plot = cowplot::plot_grid(associated_variants, spopl_region$coverage_plot, spopl_region_pu1$coverage_plot, 
                           spopl_region$tx_structure, align = "v", rel_heights = c(0.2,0.4,0.1,0.2), ncol = 1)
-ggsave("results/ATAC/QTLs/figures/SPOPL_region_combined.pdf", plot, width = 8, height = 8)
+ggsave("figures/main_figures/SPOPL_region_coverage.pdf", plot, width = 8, height = 8)
