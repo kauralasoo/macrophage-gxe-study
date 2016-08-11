c8af439cb46e23b3f138adfc0ef05ccee3f20491
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-08-11 18:07:11 +0100

    Redo variance component analysis on the gene expression data

--- SL1344/varComp/varianceComponents.R
+++ SL1344/varComp/varianceComponents.R
@@ -1,53 +1,41 @@
 #This script performs the timecomsuming bit of fitting mixed-effects
 #models on normalized gene expression data
-library("plyr")
 library("dplyr")
 library("lme4")
 library("devtools")
 library("ggplot2")
+library("purrr")
 load_all("../seqUtils/")
 
 #Import eQTL data list
-data_list = readRDS("results/SL1344/eqtl_data_list.rds")
+data_list = readRDS("results/SL1344/combined_expression_data_covariates.rds")
 
-#Perform PCA
-pca_list = performPCA(data_list$exprs_cqn, data_list$sample_metadata)
-pca_plot = ggplot(pca_list$pca_matrix, aes(x = PC1, y = PC2, 
-                                           color = condition_name, label = sample_id)) + 
-  geom_text()
-
-#Make some modifications to the sample_metadata (TODO: move this code to line_metadata construction)
-data_list$sample_metadata = dplyr::mutate(data_list$sample_metadata,
-                                          rna_auto = ifelse(chemistry == "V4_auto", "yes", "no"))
+#Construct sample metadata for VarComp analysis
+metadata = data_list$sample_metadata %>% 
+  dplyr::select(sample_id, line_id, SL1344, IFNg, rna_auto, ng_ul_mean, 
+                salmonella_date, chemistry, sex, mean_purity_filtered, passage_diff,
+                macrophage_diff_days, library_pool, rna_extraction) %>%
+  dplyr::mutate(chemistry = ifelse(chemistry == "V3", "V3", "V4")) %>%
+  dplyr::mutate(purity_bins = cut(mean_purity_filtered, breaks = c(0,0.95,0.975,1))) %>%
+  dplyr::mutate(concentration_bins = cut(ng_ul_mean, breaks = c(0,100,200,300,500))) %>%
+  dplyr::mutate(passage_diff_bins = cut(passage_diff, breaks = c(0,25,35,45,60))) %>%
+  dplyr::mutate(macrophage_diff_bins = cut(macrophage_diff_days, breaks = c(0,30,40,50,60,100)))
 
 #Prepare data for lmer analysis
-gene_ids = rownames(data_list$exprs_cqn)
+gene_ids = rownames(data_list$cqn)
 gene_id_list = idVectorToList(gene_ids)
-gene_data_list = lapply(gene_id_list, constructGeneData, 
-                        data_list$exprs_cqn, data_list$sample_metadata)
+gene_data_list = lapply(gene_id_list[1:10], constructGeneData, data_list$cqn, metadata)
 
 #Fit the extended model
 model_extended <- function(model_data){
   mod = lme4::lmer(value ~ (1|SL1344) + (1|IFNg) + (1|SL1344:IFNg) + 
-                     (1|salmonella) + (1|donor) + (1|gender) + (1|purity_bins) +
-                     (1|rna_concentration) + (1|passage_diff_bins) + (1|diff_bins) + 
-                     (1|library_pool) + (1|mf_diff_days) + (1|rna_auto), model_data)
-  return(mod)
+                     (1|line_id) + (1|rna_auto) + (1|salmonella_date) + (1|sex) + (1|chemistry) + 
+                     (1|purity_bins) + (1|concentration_bins) + (1|passage_diff_bins) + 
+                     (1|macrophage_diff_bins) + (1|library_pool) +
+                     (1|rna_extraction), model_data)
+  return(mod) 
 }
 
 variance_list = lapply(gene_data_list, estimateVarianceExplained, model_extended)
-var_table = ldply(variance_list, .id = "gene_id")
-saveRDS(var_table, "results/SL1344/varComp/model_extended_results.rds")
-
-#Fit the compact model
-#model_compact <- function(model_data){
-#  mod = lme4::lmer(value ~ (1|SL1344) + (1|IFNg) + (1|SL1344:IFNg) + 
-#                     (1|salmonella) + (1|donor) + (1|gender) + (1|purity_bins) +
-#                     (1|rna_concentration) + (1|library_pool) + (1|rna_auto), model_data)
-#  return(mod)
-#}
-
-#variance_list = lapply(gene_data_list, estimateVarianceExplained, model_compact)
-#var_table = ldply(variance_list, .id = "gene_id")
-#saveRDS(var_table, "results/SL1344/varComp/model_compact_results.rds")
-
+var_table = purrr::map_df(variance_list, identity, .id = "gene_id")
+saveRDS(var_table, "results/SL1344/varComp/model_results.rds")
