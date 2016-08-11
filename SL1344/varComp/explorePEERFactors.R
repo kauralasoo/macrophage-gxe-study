a173afc47aee6ec45cb19d2a4451eed744477455
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-08-11 18:59:04 +0100

    Remove variance component figures

--- SL1344/varComp/explorePEERFactors.R
+++ SL1344/varComp/explorePEERFactors.R
@@ -1,5 +1,4 @@
 library("devtools")
-library("plyr")
 library("dplyr")
 load_all("../seqUtils/")
 load_all("~/software/rasqual/rasqualTools/")
@@ -17,27 +16,29 @@ purity_df = dplyr::filter(sample_metadata_list$naive, mean_purity_filtered > 0.9
 peer1_plot = ggplot(purity_df, aes(x = mean_purity_filtered, y = PEER_factor_1)) + 
   geom_point() +
   xlab("Mean purity") + 
-  ylab("PEER factor 1")
-ggsave("results/SL1344/varComp/purity_vs_PEER_factor_1.pdf", peer1_plot, width = 5, height = 5)
+  ylab("PEER factor 1") +
+  theme_light()
+ggsave("figures/supplementary/varComp_purity_vs_PEER_factor_1.pdf", peer1_plot, width = 4, height = 4)
 cor(purity_df$mean_purity_filtered, purity_df$PEER_factor_1, use = "complete.obs")
 
+#RNA concetration vs PEER 1
 peer1_plot_2 = ggplot(purity_df, aes(x = ng_ul_mean, y = PEER_factor_1)) + 
   geom_point() +
-  xlab("Cell density (rna concentration)") + 
-  ylab("PEER factor 1")
-ggsave("results/SL1344/varComp/rna_concetration_vs_PEER_factor_1.pdf", peer1_plot_2, width = 5, height = 5)
+  xlab("RNA concentration (ng/ul)") + 
+  ylab("PEER factor 1") + 
+  theme_light()
+ggsave("figures/supplementary/varComp_rna_concetration_vs_PEER_factor_1.pdf", peer1_plot_2, width = 4, height = 4)
+cor.test(purity_df$ng_ul_mean, purity_df$PEER_factor_1, use = "complete.obs")
 
 #Focus on the relationship between sequencing chemistry and first PEER factor
-chemistry_df = dplyr::mutate(sample_metadata_list$naive, rna_auto = ifelse(chemistry == "V4_auto", "automatic", "manual"))
-peer2_plot = ggplot(chemistry_df, aes(x = rna_auto, y = PEER_factor_2)) + 
+peer2_plot = ggplot(sample_metadata_list$naive, aes(x = rna_auto, y = PEER_factor_2)) + 
   geom_boxplot(outlier.shape = NA) +
   geom_jitter(position = position_jitter(width = .1)) + 
-  xlab("RNA sequening chemistry") + 
-  ylab("PEER factor 2")
-ggsave("results/SL1344/varComp/purity_vs_PEER_factor_2.pdf", peer2_plot, width = 5, height = 5)
-cor(as.numeric(factor(chemistry_df$rna_auto)) , chemistry_df$PEER_factor_2, use = "complete.obs")
-
-
+  xlab("Automatic RNA-seq") + 
+  ylab("PEER factor 2") +
+  theme_light()
+ggsave("figures/supplementary/varComp_autmatic_vs_PEER_factor_2.pdf", peer2_plot, width = 4, height = 4)
+cor.test(as.numeric(sample_metadata_list$naive$rna_auto) , sample_metadata_list$naive$PEER_factor_2, use = "complete.obs")
 
 #### Look at the variation explained by each PEER factor ####
 prec_naive = read.table("results/SL1344/PEER/naive_10/precision.txt", sep = ",")
@@ -58,7 +59,11 @@ precision_df = dplyr::mutate(precision_df, factor = paste("PEER_factor_", c(1:10
 precision_plot = ggplot(precision_df, aes(x = factor, y = var_exp, color = condition, group = condition)) +
   geom_point() + 
   geom_line() +
-  ylab("Fraction variance explained") + 
+  ylab("Fraction variance explained") +
+  theme_light() +
   theme(axis.text.x = element_text(angle = 25))
 precision_plot
-ggsave("results/SL1344/varComp/PEER_explained.pdf", precision_plot, width = 7, height = 5)
+ggsave("figures/supplementary/varComp_PEER_explained.pdf", precision_plot, width = 6, height = 5)
+
+
+
