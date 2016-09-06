4aba46e00bd10b11984fc1d6a3340c8c674820d9
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-09-06 10:32:34 +0300

    Move shared scripts to shared munge folder

--- SL1344/munge/diluteRNA.R
+++
@@ -1,5 +0,0 @@
-nanodrop = read.table("macrophage-gxe-study/data/RNA_extractions/RNA_extractions_270815.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
-bioanalyzer = read.table("macrophage-gxe-study/data/bioanalyzer/bioanalyzer_samples_2.txt", header = TRUE, stringsAsFactors = FALSE)
-
-combined = dplyr::select(nanodrop, sample_id, ng.ul) %>% dplyr::left_join(bioanalyzer, by = "sample_id")
-write.table(combined, "results/SL1344/nano_bio.txt", sep = "\t", quote = FALSE)
