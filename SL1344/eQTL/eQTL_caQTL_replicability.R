c1d6811fa01301d4d4802cb47d451abe9cb8a938
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-11-09 11:54:01 +0000

    Update replicability plots

--- SL1344/eQTL/eQTL_caQTL_replicability.R
+++ SL1344/eQTL/eQTL_caQTL_replicability.R
@@ -45,6 +45,7 @@ concordance_df = dplyr::bind_rows(rna_rasqual_concordance, rna_fastqtl_100kb_con
                                   rna_fastqtl_500kb_concordance, atac_rasqual_concordance,
                                   atac_fastqtl_50kb_concordance, atac_fastqtl_100kb_concordance)
 saveRDS(concordance_df, "results/ATAC_RNA_overlaps/concordance_df.rds")
+concordance_df = readRDS("results/ATAC_RNA_overlaps/concordance_df.rds")
 
 #Make a concordance plot
 concordance_df_clean = dplyr::mutate(concordance_df, 
@@ -53,12 +54,34 @@ concordance_plot = ggplot(concordance_df_clean, aes(x = method, y = concordance)
                                        facet_wrap(~class, scales = "free_x") + 
                                        geom_boxplot() + geom_point() + theme_light() +
                                        ylab("Lead variant concordance") +
-                                       scale_y_continuous(limits = c(0,0.8)) +
+                                       scale_y_continuous(limits = c(0,1)) +
                                        theme(axis.text.x = element_text(angle = 50, hjust = 1)) 
 ggsave("figures/main_figures/concordance_between_conditions_atac_rna.pdf", concordance_plot, width = 6, height = 4)
 
 #Calculate Pi1 replicability values
+rna_fastqtl_100kb_pi1 = calculatePairwisePi1(rna_fastqtl_100kb, tidy = T) %>%
+  dplyr::filter(first != second) %>% 
+  dplyr::mutate(method = "FastQTL (100kb)", class = "eQTL")
+rna_fastqtl_500kb_pi1 = calculatePairwisePi1(rna_fastqtl_500kb, tidy = T) %>%
+  dplyr::filter(first != second) %>% 
+  dplyr::mutate(method = "FastQTL (500kb)", class = "eQTL")
 
+atac_fastqtl_100kb_pi1 = calculatePairwisePi1(atac_fastqtl_100kb, tidy = T) %>%
+  dplyr::filter(first != second) %>% 
+  dplyr::mutate(method = "FastQTL (100kb)", class = "caQTL")
+atac_fastqtl_50kb_pi1 = calculatePairwisePi1(atac_fastqtl_50kb, tidy = T) %>%
+  dplyr::filter(first != second) %>% 
+  dplyr::mutate(method = "FastQTL (50kb)", class = "caQTL")
 
-
+#Combine into a df and make a plot
+pi1_df = dplyr::bind_rows(rna_fastqtl_100kb_pi1, rna_fastqtl_500kb_pi1,
+                                  atac_fastqtl_50kb_pi1, atac_fastqtl_100kb_pi1) %>%
+  dplyr::mutate(class = factor(class, levels = c("eQTL","caQTL")))
+pi1_plot = ggplot(pi1_df, aes(x = method, y = pi1)) + 
+  facet_wrap(~class, scales = "free_x") + 
+  geom_boxplot() + geom_jitter(width = 0.1) + theme_light() +
+  ylab(expression(paste("Pairwise ", pi[1], " statistic"))) +
+  scale_y_continuous(limits = c(0,1)) +
+  theme(axis.text.x = element_text(angle = 50, hjust = 1)) 
+ggsave("figures/main_figures/pi1_between_conditions_atac_rna.pdf", pi1_plot, width = 6, height = 4)
 
