02fc0028c52086abc6dc621f9a64a7eb0aa0621b
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-11-10 00:07:15 +0000

    Calculate z-scores and LD for all eQTLs

--- ATAC_RNA/alleleic_heterogeneity.R
+++ ATAC_RNA/alleleic_heterogeneity.R
@@ -1,8 +1,26 @@
 library("devtools")
 library("plyr")
 library("dplyr")
+library("purrr")
 load_all("../seqUtils/")
+load_all("macrophage-gxe-study/housekeeping/")
 load_all("~/software/rasqual/rasqualTools/")
+
+#Helper functions
+pToZ <- function(pvalue_df, n_top_snps = 200){
+  result = pvalue_df %>% 
+    dplyr::arrange(p_nominal) %>%
+    head(n_top_snps) %>% 
+    dplyr::arrange(pos) %>%
+    dplyr::mutate(z = qnorm(p_nominal)*sign(beta)) %>%
+    dplyr::select(snp_id, z)
+  return(result)
+}
+
+zToLD <- function(z_df, genotypes){
+  cor_mat = cor(t(genotypes[z_df$snp_id,]), method = "pearson", use = "pairwise.complete.obs")
+  return(cor_mat)
+}
 
 #Load the raw eQTL dataset
 combined_expression_data = readRDS("results/SL1344/combined_expression_data_covariates.rds")
@@ -19,6 +37,19 @@ vcf_file = readRDS("genotypes/SL1344/imputed_20151005/imputed.86_samples.sorted.
 
 #Import variant information
 snp_info = importVariantInformation("genotypes/SL1344/imputed_20151005/imputed.86_samples.variant_information.txt.gz")
+
+#Import QTL lists
+rna_fastqtl_100kb = readRDS("results/SL1344/eQTLs/fastqtl_min_pvalues_100kb.rds")
+naive_eqtls = dplyr::filter(rna_fastqtl_100kb$naive, p_nominal < 1e-8) %>% dplyr::select(gene_id)
+naive_eqtl_ranges = constructGeneRanges(naive_eqtls, tss_coords, 1e5)
+rna_pvals = fastqtlTabixFetchGenes(naive_eqtl_ranges, qtlResults()$rna_fastqtl$naive)
+
+#Convert p-values to z-scores and compute LD matrices
+rna_z = purrr::map(rna_pvals, pToZ)
+rna_z = rna_z[unlist(map(rna_z, nrow)) > 0] #Remove genes with 0 variants
+rna_ld = purrr::map(rna_z, zToLD, vcf_file$genotypes)
+
+
 
 #Construct gene ranges
 ptk2b_region = constructGeneRanges(data_frame(gene_id = "ENSG00000120899"), tss_coords, 2e5)
