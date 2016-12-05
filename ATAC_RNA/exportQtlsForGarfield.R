b309930b426b76141d55de19370d867d67e9584c
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-12-05 23:31:41 +0000

    Export full caQTLs for GARFIELD

--- ATAC_RNA/exportQtlsForGarfield.R
+++ ATAC_RNA/exportQtlsForGarfield.R
@@ -6,12 +6,12 @@ load_all("../seqUtils/")
 load_all("~/software/rasqual/rasqualTools/")
 load_all("macrophage-gxe-study/housekeeping/")
 
+#Import GRCh37 variant information
+GRCh37_variants = importVariantInformation("genotypes/SL1344/imputed_20151005/GRCh37/imputed.86_samples.variant_information.GRCh37.vcf.gz")
+
 #Import eQTLs
 combined_expression_data = readRDS("results/SL1344/combined_expression_data_covariates.rds")
 rasqual_min_pvalues = readRDS("results/SL1344/eQTLs/rasqual_min_pvalues.rds")
-
-#Import GRCh37 variant information
-GRCh37_variants = importVariantInformation("genotypes/SL1344/imputed_20151005/GRCh37/imputed.86_samples.variant_information.GRCh37.vcf.gz")
 
 #Iterate over chromosomes
 chromosomes = c(1:22) %>% as.character()
@@ -33,6 +33,34 @@ for (chr in chromosomes){
               row.names = FALSE, col.names = FALSE)
   
 }
+
+#Import caQTLs
+atac_data = readRDS("results/ATAC/ATAC_combined_accessibility_data_covariates.rds")
+rasqual_min_pvalues = readRDS("results/ATAC/QTLs/rasqual_min_pvalues.rds")
+
+#Iterate over chromosomes
+chromosomes = c(1:22) %>% as.character()
+for (chr in chromosomes){
+  chr_string = paste0("chr", chr)
+  print(chr_string)
+  
+  #Import Garfield coords
+  variant_coords_dir = "databases/garfield-data/annotations/variant_coords/"
+  coords = readr::read_tsv(file.path(variant_coords_dir, chr_string), col_names = "pos", col_types = "i")
+  
+  #Process summary stats
+  garfield_results = rasqualSummariesToGarfieldByChr(chr, rasqual_min_pvalues, qtlResults()$atac_rasqual, 
+                                                     GRCh37_variants, atac_data$gene_metadata, coords)
+  
+  #Export annotations
+  output_dir = "databases/garfield-data/annotation_caqtl/"
+  write.table(garfield_results, file.path(output_dir, chr_string), sep = "", quote = FALSE, 
+              row.names = FALSE, col.names = FALSE)
+  
+}
+
+
+
 
 #Export only lead eQTL and caQTL variants for GARFIELD
 eQTL_rasqual_min_pvalues = readRDS("results/SL1344/eQTLs/rasqual_min_pvalues.rds")
