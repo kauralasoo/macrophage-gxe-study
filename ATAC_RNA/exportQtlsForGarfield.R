af28f7b6706ab3a73b265fcd42d6f5e92b85f7bf
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-12-06 14:42:25 +0000

    Remove lead variant export for GARFIELD analysis

--- ATAC_RNA/exportQtlsForGarfield.R
+++ ATAC_RNA/exportQtlsForGarfield.R
@@ -50,7 +50,7 @@ for (chr in chromosomes){
   
   #Process summary stats
   garfield_results = rasqualSummariesToGarfieldByChr(chr, rasqual_min_pvalues, qtlResults()$atac_rasqual, 
-                                                     GRCh37_variants, atac_data$gene_metadata, coords)
+                                                     GRCh37_variants, atac_data$gene_metadata, coords, cis_dist = 1e5)
   
   #Export annotations
   output_dir = "databases/garfield-data/annotation_caqtl/"
@@ -59,62 +59,10 @@ for (chr in chromosomes){
   
 }
 
-
-
-
-#Export only lead eQTL and caQTL variants for GARFIELD
-eQTL_rasqual_min_pvalues = readRDS("results/SL1344/eQTLs/rasqual_min_pvalues.rds")
-names(eQTL_rasqual_min_pvalues) = paste0(names(eQTL_rasqual_min_pvalues), "_eQTL")
-
-caQTL_rasqual_min_pvalues = readRDS("results/ATAC/QTLs/rasqual_min_pvalues.rds")
-names(caQTL_rasqual_min_pvalues) = paste0(names(caQTL_rasqual_min_pvalues), "_caQTL")
-
-#Merge all QTLs
-all_qtls_snps = c(eQTL_rasqual_min_pvalues, caQTL_rasqual_min_pvalues) %>% 
-  purrr::map(~dplyr::filter(., p_fdr < 0.1) %>% dplyr::select(snp_id) %>% unique())
-
-#Add GRCh37 coordinates
-lead_variants = purrr::map_df(all_qtls_snps, identity) %>% unique()
-selected_variants = dplyr::filter(GRCh37_variants, snp_id %in% lead_variants$snp_id) %>% 
-  dplyr::select(chr, pos, snp_id)
-
-#Add variants
-all_qtl_pos = purrr::map(all_qtls_snps, ~dplyr::left_join(., selected_variants, by = "snp_id") %>%
-                           dplyr::filter(!is.na(pos)) %>%
-                           dplyr::mutate(is_qtl = 1))
-
-#Extract all variants
-all_variants = purrr::map_df(all_qtl_pos, identity) %>% unique() %>%
-  dplyr::select(snp_id, chr, pos)
-
-#Merge all lists into a single df
-reduced_df = purrr::reduce(all_qtl_pos, dplyr::left_join, .init = all_variants, by = c("snp_id", "chr", "pos"))
-names(reduced_df)[4:11] = names(all_qtl_pos)
-reduced_df[is.na(reduced_df)] = 0
-
-#Iterate over chromosomes and save results
-#Iterate over chromosomes
-chromosomes = c(1:22) %>% as.character()
-for (chr in chromosomes){
-  chr_string = paste0("chr", chr)
-  print(chr_string)
-  
-  #Extract chromosome data 
-  chr_df = extractChrFromReducedDf(chr, reduced_df)
-  
-  output_dir = "databases/garfield-data/annotation/"
-  write.table(chr_df, file.path(output_dir, chr_string), sep = "", quote = FALSE, 
-              row.names = FALSE, col.names = FALSE)
-}
-
 #Make a link file
-link_file = data_frame(Index = c(0:7), annotation = names(all_qtl_pos), 
-           Celltype = "Macrophage", "Tissue" = "Blood", 
-           Type = c(rep("eQTL",4), rep("caQTL",4)), 
-           Category = "QTL")
+link_file = data_frame(Index = c(0:3), annotation = names(rasqual_min_pvalues), 
+                       Celltype = "Macrophage", "Tissue" = "Blood", 
+                       Type = rep("caQTL",4), 
+                       Category = "QTL")
 write.table(link_file, "databases/garfield-data/annotation/link_file.txt", quote = FALSE,
             sep = " ", row.names = FALSE, col.names = TRUE)
-
-
-
-
