ffc3735fdad214f4d452857a3628d535bce6e51b
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-12-10 17:51:54 +0000

    Redraw GARFIELD enrichment plot

--- ATAC_RNA/figures/garfield_enrichment.R
+++ ATAC_RNA/figures/garfield_enrichment.R
@@ -1,74 +1,60 @@
 library("ggplot2")
+library("ggrepel")
+library("dplyr")
+library("devtools")
+load_all("macrophage-gxe-study/housekeeping/")
+
 
 #Import GWAS summary list
 gwas_stats_labeled = readr::read_tsv("macrophage-gxe-study/data/gwas_catalog/GWAS_summary_stat_list.labeled.txt",
                                 col_names = c("trait","file_name"))
 
-##### eQTL enrichments #####
+##### import eQTL enrichments #####
 #Name all files
 file_names = as.list(paste0("results/SL1344/coloc/GARFIELD/eQTL/garfield.perm.",gwas_stats_labeled$file_name, ".out"))
 names(file_names) = gwas_stats_labeled$trait
 
 #Import enrichments
 enrich_df = purrr::map_df(file_names, ~readr::read_delim(., delim = " "), .id = "trait")
-filtered_enrichments = dplyr::filter(enrich_df, PThresh %in% c(1e-8,1e-5)) %>%
+eqtl_filtered_enrichments = dplyr::filter(enrich_df, PThresh == 1e-5) %>%
   dplyr::filter(!(trait %in% c("UC_2014","UC_2012", "CEL_2010","PS", "RA_2012", "CD_2012", "T1D_2", "T2D_1", "PBC","UC","CD")))
 
-#Low threshold
-low_enrichments = dplyr::filter(filtered_enrichments, PThresh == 1e-5)
-
-#Calculate max fold enrichments
-mean_fe_df = dplyr::group_by(low_enrichments, trait) %>% 
-  dplyr::mutate(mean_FE = mean(FE)) %>% arrange(mean_FE) %>%
-  dplyr::ungroup()
-ranked_enrichments = dplyr::mutate(low_enrichments, trait = factor(trait, levels = unique(mean_fe_df$trait)))
-
-#Make plots
-enrichment_plot = ggplot(ranked_enrichments, aes(x = log(FE,2), y = trait, color = Annotation)) + 
-  geom_point() +
-  scale_x_continuous(limits = c(1, 6)) + 
-  theme_light()
-ggsave("figures/main_figures/grafield_enrichment-5.pdf", plot = enrichment_plot, width = 5, height = 4)
-
-enrichment_plot = ggplot(ranked_enrichments, aes(x = FE, y = trait, color = Annotation)) + 
-  geom_point() +
-  scale_x_continuous(limits = c(0, 41)) + 
-  theme_light()
-ggsave("figures/main_figures/grafield_enrichment_FE-5.pdf", plot = enrichment_plot, width = 5, height = 4)
-
-
-
-##### caQTL enrichments #####
+##### import caQTL enrichments #####
 #Name all files
 file_names = as.list(paste0("results/SL1344/coloc/GARFIELD/caQTL/garfield.perm.",gwas_stats_labeled$file_name, ".out"))
 names(file_names) = gwas_stats_labeled$trait
 
 #Import enrichments
 enrich_df = purrr::map_df(file_names, ~readr::read_delim(., delim = " "), .id = "trait")
-filtered_enrichments = dplyr::filter(enrich_df) %>%
-  dplyr::filter(!(trait %in% c("UC_2014","UC_2012", "CEL_2010","PS", "RA_2012", "CD_2012", "T1D_2", "T2D_1", "PBC","UC","CD")))
+caqtl_filtered_enrichments = dplyr::filter(enrich_df, PThresh == 1e-5) %>%
+  dplyr::filter(!(trait %in% c("UC_2014","UC_2012", "CEL_2010","PS", "RA_2012", "CD_2012", "T1D_2", "T2D_1", "PBC","UC","CD"))) %>%
+  dplyr::rename(Annotation = annotation)
 
-
-#Low threshold
-low_enrichments = dplyr::filter(filtered_enrichments, PThresh == 1e-5)
+# Combine eQTL and caQTL enrichments
+joint_enrichments = dplyr::bind_rows(eqtl_filtered_enrichments, caqtl_filtered_enrichments) %>%
+  dplyr::mutate(EmpPval = ifelse(EmpPval == 0, 1e-5, EmpPval)) %>%
+  dplyr::mutate(log10p = -log(EmpPval, 10)) %>%
+  tidyr::separate(Annotation, into = c("condition_name", "qtl_type"), sep ="_e|_c") %>%
+  dplyr::select(-qtl_type) %>%
+  dplyr::left_join(figureNames(), by = "condition_name") %>%
+  dplyr::mutate(phenotype = ifelse(Type == "eQTL", "RNA-seq", "ATAC-seq"))
 
 #Calculate max fold enrichments
-mean_fe_df = dplyr::group_by(low_enrichments, trait) %>% 
+mean_fe_df = dplyr::group_by(joint_enrichments, trait) %>% 
   dplyr::mutate(mean_FE = mean(FE)) %>% arrange(mean_FE) %>%
   dplyr::ungroup()
-ranked_enrichments = dplyr::mutate(low_enrichments, trait = factor(trait, levels = unique(mean_fe_df$trait)))
+ranked_enrichments = dplyr::mutate(joint_enrichments, trait = factor(trait, levels = unique(mean_fe_df$trait)))
 
 #Make plots
-enrichment_plot = ggplot(ranked_enrichments, aes(x = log(FE,2), y = trait, color = annotation)) + 
-  geom_point() +
-  scale_x_continuous(limits = c(0, 6)) + 
-  theme_light()
-ggsave("figures/main_figures/grafield_caQTL_enrichment-5.pdf", plot = enrichment_plot, width = 5, height = 4)
-
-enrichment_plot = ggplot(ranked_enrichments, aes(x = FE, y = trait, color = annotation)) + 
-  geom_point() +
+enrichment_plot = ggplot(ranked_enrichments, aes(x = FE, y = trait, color = figure_name, size = log10p)) + 
+  geom_jitter(height = 0, width = 2) +
   scale_x_continuous(limits = c(0, 41)) + 
-  theme_light()
-ggsave("figures/main_figures/grafield_caQTL_enrichment_FE-5.pdf", plot = enrichment_plot, width = 5, height = 4)
+  theme_light() +
+  scale_size(range = c(1,4), name = "-log10\np-value") +
+  facet_wrap(~phenotype) +
+  xlab("Fold enrichment") +
+  ylab("Trait") +
+  scale_color_manual(values = conditionPalette(), name="Condition")
+ggsave("figures/main_figures/grafield_enrichment.pdf", plot = enrichment_plot, width = 4.5, height = 3.5)
 
 
