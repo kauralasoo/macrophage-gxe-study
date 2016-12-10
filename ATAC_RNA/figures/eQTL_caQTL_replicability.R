bbf16424a65aa64efc875452033bffe037de93ee
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-12-10 15:19:02 +0000

    Redo Pi1 and lead variant concordance figures.

--- ATAC_RNA/figures/eQTL_caQTL_replicability.R
+++ ATAC_RNA/figures/eQTL_caQTL_replicability.R
@@ -47,41 +47,63 @@ concordance_df = dplyr::bind_rows(rna_rasqual_concordance, rna_fastqtl_100kb_con
 saveRDS(concordance_df, "results/ATAC_RNA_overlaps/concordance_df.rds")
 concordance_df = readRDS("results/ATAC_RNA_overlaps/concordance_df.rds")
 
+#Make a clean concordance df
+concordance_df_clean = tidyr::separate(concordance_df, method, into = c("method", "window"), sep = " ") %>% 
+  dplyr::mutate(phenotype = ifelse(class == "caQTL", "ATAC-seq", "RNA-seq"))
+
 #Make a concordance plot
-concordance_df_clean = dplyr::mutate(concordance_df, 
-            class = factor(class, levels = c("eQTL","caQTL")))
-concordance_plot = ggplot(concordance_df_clean, aes(x = method, y = concordance)) + 
-                                       facet_wrap(~class, scales = "free_x") + 
-                                       geom_boxplot() + geom_point() + theme_light() +
-                                       ylab("Lead variant concordance") +
-                                       scale_y_continuous(limits = c(0,1)) +
-                                       theme(axis.text.x = element_text(angle = 50, hjust = 1)) 
-ggsave("figures/main_figures/concordance_between_conditions_atac_rna.pdf", concordance_plot, width = 6, height = 4)
+concordance_filtered = dplyr::filter(concordance_df_clean, window != "(100kb)")
+concordance_plot = ggplot(concordance_filtered, aes(x = method, y = concordance, color = method)) + 
+  geom_boxplot() + 
+  geom_point(position = position_jitter(width = 0.2)) + 
+  facet_wrap(~phenotype) +
+  ylab("Lead variant concordance") +
+  scale_color_manual(values = c("#a6cee3", "#1f78b4")) +
+  scale_y_continuous(limits = c(0,1)) + 
+  theme_light() +
+  theme(legend.position = "top", axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
+ggsave("figures/main_figures/concordance_between_conditions_atac_rna.pdf", plot = concordance_plot, width = 4, height = 3.5)
+
+#Make a concordance plot in 100kb cis window
+concordance_filtered_100kb = dplyr::filter(concordance_df_clean, window == "(100kb)") %>%
+  dplyr::mutate(window = gsub("(", "", window, fixed = TRUE)) %>%
+  dplyr::mutate(window = gsub(")", "", window, fixed = TRUE))
+concordance_plot_100kb = ggplot(concordance_filtered_100kb, aes(x = window, y = concordance)) + 
+  geom_boxplot() + 
+  geom_point(position = position_jitter(width = 0.2)) + 
+  facet_wrap(~phenotype) +
+  ylab("Lead variant concordance") +
+  xlab("Cis-window size") +
+  scale_y_continuous(limits = c(0,1)) + 
+  theme_light()
+ggsave("figures/supplementary/concordance_between_conditions_atac_rna_100kb_window.pdf", plot = concordance_plot_100kb, width = 3, height = 3.5)
+
+
 
 #Calculate Pi1 replicability values
 rna_fastqtl_100kb_pi1 = calculatePairwisePi1(rna_fastqtl_100kb, tidy = T) %>%
   dplyr::filter(first != second) %>% 
-  dplyr::mutate(method = "FastQTL (100kb)", class = "eQTL")
+  dplyr::mutate(window_size = "100kb", class = "RNA-seq")
 rna_fastqtl_500kb_pi1 = calculatePairwisePi1(rna_fastqtl_500kb, tidy = T) %>%
   dplyr::filter(first != second) %>% 
-  dplyr::mutate(method = "FastQTL (500kb)", class = "eQTL")
+  dplyr::mutate(window_size ="500kb", class = "RNA-seq")
 
 atac_fastqtl_100kb_pi1 = calculatePairwisePi1(atac_fastqtl_100kb, tidy = T) %>%
   dplyr::filter(first != second) %>% 
-  dplyr::mutate(method = "FastQTL (100kb)", class = "caQTL")
+  dplyr::mutate(window_size = "100kb" , class = "ATAC-seq")
 atac_fastqtl_50kb_pi1 = calculatePairwisePi1(atac_fastqtl_50kb, tidy = T) %>%
   dplyr::filter(first != second) %>% 
-  dplyr::mutate(method = "FastQTL (50kb)", class = "caQTL")
+  dplyr::mutate(window_size = "50kb", class = "ATAC-seq")
 
 #Combine into a df and make a plot
 pi1_df = dplyr::bind_rows(rna_fastqtl_100kb_pi1, rna_fastqtl_500kb_pi1,
                                   atac_fastqtl_50kb_pi1, atac_fastqtl_100kb_pi1) %>%
-  dplyr::mutate(class = factor(class, levels = c("eQTL","caQTL")))
-pi1_plot = ggplot(pi1_df, aes(x = method, y = pi1)) + 
+  dplyr::mutate(window_size = factor(window_size, levels = c("50kb", "100kb", "500kb")))
+pi1_plot = ggplot(pi1_df, aes(x = window_size, y = pi1)) + 
   facet_wrap(~class, scales = "free_x") + 
-  geom_boxplot() + geom_jitter(width = 0.1) + theme_light() +
+  geom_boxplot(outlier.shape = NA) + geom_jitter(width = 0.2) + theme_light() +
   ylab(expression(paste("Pairwise ", pi[1], " statistic"))) +
   scale_y_continuous(limits = c(0,1)) +
-  theme(axis.text.x = element_text(angle = 50, hjust = 1)) 
-ggsave("figures/main_figures/pi1_between_conditions_atac_rna.pdf", pi1_plot, width = 6, height = 4)
+  xlab("Cis-window size")
+ggsave("figures/supplementary/pi1_between_conditions_atac_rna.pdf", pi1_plot, width = 4, height = 3.5)
 
