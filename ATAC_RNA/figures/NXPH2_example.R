b062c52ac05d13179fb8704bf67f03a2fe545acf
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-12-15 13:49:47 +0000

    Make more plots for the NXPH2 locus

--- ATAC_RNA/figures/NXPH2_example.R
+++ ATAC_RNA/figures/NXPH2_example.R
@@ -23,6 +23,7 @@ atac_meta_df = wiggleplotrConstructMetadata(atac_list$counts, atac_list$sample_m
 
 #Import the VCF file
 vcf_file = readRDS("genotypes/SL1344/imputed_20151005/imputed.86_samples.sorted.filtered.named.rds")
+variant_information = importVariantInformation("../macrophage-gxe-study/genotypes/SL1344/imputed_20151005/imputed.86_samples.variant_information.txt.gz")
 
 
 #Import transcript annotations and metadata
@@ -92,12 +93,22 @@ ggsave("figures/main_figures/NXPH2_manhattan_plots.png", plot = joint_plot, widt
 
 
 #### eQTL boxplot ####
-#Make eQTL boxplot
+#Make eQTL boxplots
 NXPH2_data = constructQtlPlotDataFrame("ENSG00000144227", "rs7594476", combined_expression_data$cqn, vcf_file$genotypes, 
                                       combined_expression_data$sample_metadata, combined_expression_data$gene_metadata) %>%
-  dplyr::filter(condition_name %in% c("naive","IFNg"))
+  dplyr::filter(condition_name %in% c("naive","IFNg")) %>%
+  dplyr::left_join(constructGenotypeText("rs7594476", variant_information), by = "genotype_value")
 NXPH2_plot = plotQtlCol(NXPH2_data)
 ggsave("figures/main_figures/NXPH2_expression_boxplot.pdf", plot = NXPH2_plot, width = 2.5, height = 3.5)
+
+#SPOPL gene
+SPOPL_data = constructQtlPlotDataFrame("ENSG00000144228", "rs7594476", combined_expression_data$cqn, vcf_file$genotypes, 
+                                       combined_expression_data$sample_metadata, combined_expression_data$gene_metadata) %>%
+  dplyr::filter(condition_name %in% c("naive","IFNg")) %>%
+  dplyr::left_join(constructGenotypeText("rs7594476", variant_information), by = "genotype_value")
+SPOPL_plot = plotQtlCol(SPOPL_data)
+ggsave("figures/main_figures/SPOPL_expression_boxplot.pdf", plot = SPOPL_plot, width = 2.5, height = 3.5)
+
 
 
 #Make a coverage plot of the whole SPOPL-NXPH2 locus
@@ -122,5 +133,39 @@ ATAC_coverage = plotCoverage(exons = peak_annot$peak_list, cdss = peak_annot$pea
 
 joint_plot = cowplot::plot_grid(ATAC_coverage$coverage_plot, tx_plot, 
                                 align = "v", ncol = 1, rel_heights = c(4,2))
-ggsave("figures/test.png", joint_plot, width = 5, height = 3.5)
+ggsave("figures/main_figures/NXPH2_full_region.png", joint_plot, width = 8, height = 3)
+
+
+
+#Make a focussed coverage plot from the main peak region
+region_coords = c(138688000, 138695000)
+peak_annot = wiggleplotrExtractPeaks(region_coords, chrom = 2, atac_list$gene_metadata)
+peak_annot$peak_annot$gene_name = ""
+
+#Make ATAC coverage plot
+atac_track_data = wiggleplotrGenotypeColourGroup(atac_meta_df, "rs7594476", vcf_file$genotypes, 1) %>%
+  dplyr::filter(track_id %in% c("naive", "IFNg"))
+ATAC_coverage = plotCoverage(exons = peak_annot$peak_list, cdss = peak_annot$peak_list, track_data = atac_track_data, rescale_introns = FALSE, 
+                             transcript_annotations = peak_annot$peak_annot, fill_palette = getGenotypePalette(), 
+                             connect_exons = FALSE, label_type = "peak", plot_fraction = 0.2, heights = c(0.7,0.3), 
+                             region_coords = region_coords, return_subplots_list = TRUE, coverage_type = "both")
+
+#Make a plot of the PU.1 ChIP-Seq data
+pu1_df = data_frame(sample_id = "PU1_naive", track_id = "PU.1", colour_group = "PU.1", scaling_factor = 10) %>%
+  dplyr::mutate(bigWig = file.path("/Volumes/JetDrive/bigwigs/Schmidt", paste(sample_id, ".bw", sep = "")))
+spopl_region_pu1 = plotCoverage(exons = peak_annot$peak_list, cdss = peak_annot$peak_list, track_data = pu1_df, rescale_introns = FALSE, 
+                                transcript_annotations = peak_annot$peak_annot, fill_palette = c("black"), 
+                                connect_exons = FALSE, label_type = "peak", plot_fraction = 0.3, heights = c(0.5,0.5), 
+                                region_coords = region_coords, return_subplots_list = TRUE)
+
+#Make manhattan plot
+pvals = makeManhattanPlot(dplyr::filter(peak_pvalues, track_id == "IFNg"), region_coords, color_R2 = TRUE)
+
+joint_plot = cowplot::plot_grid(pvals, ATAC_coverage$coverage_plot, spopl_region_pu1$coverage_plot, ATAC_coverage$tx_structure, 
+                                align = "v", ncol = 1, rel_heights = c(1.5,1.5,.7,1))
+ggsave("figures/main_figures/NXPH2_finemapping.pdf", plot = joint_plot, width = 4, height = 3.5)
+
+
+
+
 
