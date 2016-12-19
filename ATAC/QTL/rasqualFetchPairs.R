3d38fc4c041cae36410e65374423d4a5a374e899
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-12-19 23:10:13 +0000

    Redo condition-specific caQTL testing using TPM values (less inflation)

--- ATAC/QTL/rasqualFetchPairs.R
+++ ATAC/QTL/rasqualFetchPairs.R
@@ -6,31 +6,20 @@ load_all("~/software/rasqual/rasqualTools/")
 
 #Import SNP-peak pairs
 rasqual_min_pvalues = readRDS("results/ATAC/QTLs/rasqual_min_pvalues.rds")
-min_pvalue_hits = lapply(rasqual_min_pvalues, function(x){dplyr::filter(x, p_fdr < 0.1)})
-min_pvalues_df = ldply(min_pvalue_hits, .id = "condition_name")
+min_pvalue_hits = lapply(rasqual_min_pvalues, function(x){dplyr::filter(x, p_eigen < fdr_thresh)})
+min_pvalues_df = purrr::map_df(min_pvalue_hits, identity, .id = "condition_name") %>%
+  dplyr::arrange(gene_id, p_nominal)
 joint_pairs = dplyr::select(min_pvalues_df, gene_id, snp_id) %>% unique()
 
-#Import SNP coords
-snp_coords = readr::read_delim("../macrophage-gxe-study/genotypes/SL1344/imputed_20151005/imputed.86_samples.variant_information.txt.gz", 
-                               delim = "\t", col_types = "cdccc", col_names = c("chr","pos","snp_id","ref","alt"))
+#Import variant information
+snp_coords = importVariantInformation("genotypes/SL1344/imputed_20151005/imputed.86_samples.variant_information.txt.gz")
 selected_snp_coords = dplyr::filter(snp_coords, snp_id %in% unique(joint_pairs$snp_id)) %>% 
   dplyr::transmute(seqnames = chr, start = pos, end = pos, snp_id, strand = "*") %>%
   dataFrameToGRanges()
 unique_genes = unique(joint_pairs$gene_id)
 
-#Import SNP-peak pairs from disk
-naive_snp_res = tabixFetchSNPs(selected_snp_coords, "results/ATAC/rasqual/output/naive_100kb/naive_100kb.sorted.txt.gz") %>% 
-  dplyr::filter(gene_id %in% unique_genes) %>% 
-  dplyr::semi_join(joint_pairs, by = c("gene_id", "snp_id"))
-IFNg_snp_res = tabixFetchSNPs(selected_snp_coords, "results/ATAC/rasqual/output/IFNg_100kb/IFNg_100kb.sorted.txt.gz") %>% 
-  dplyr::filter(gene_id %in% unique_genes) %>% 
-  dplyr::semi_join(joint_pairs, by = c("gene_id", "snp_id"))
-SL1344_snp_res = tabixFetchSNPs(selected_snp_coords, "results/ATAC/rasqual/output/SL1344_100kb/SL1344_100kb.sorted.txt.gz") %>% 
-  dplyr::filter(gene_id %in% unique_genes) %>% 
-  dplyr::semi_join(joint_pairs, by = c("gene_id", "snp_id"))
-IFNg_SL1344_snp_res = tabixFetchSNPs(selected_snp_coords, "results/ATAC/rasqual/output/IFNg_SL1344_100kb/IFNg_SL1344_100kb.sorted.txt.gz") %>% 
-  dplyr::filter(gene_id %in% unique_genes) %>% 
-  dplyr::semi_join(joint_pairs, by = c("gene_id", "snp_id"))
-
-rasqual_selected_results = list(naive = naive_snp_res, IFNg = IFNg_snp_res, SL1344 = SL1344_snp_res, IFNg_SL1344 = IFNg_SL1344_snp_res)
-saveRDS(rasqual_selected_results, "results/ATAC/QTLs/rasqual_selected_results.rds")
+#Import RASQUAL summary stats for SNP-peak pairs from disk
+snp_res_list = purrr::map(qtlResults()$atac_rasqual, ~tabixFetchSNPs(selected_snp_coords, .) %>%
+                            dplyr::filter(gene_id %in% unique_genes) %>%
+                            dplyr::semi_join(joint_pairs, by = c("gene_id", "snp_id")))
+saveRDS(snp_res_list, "results/ATAC/QTLs/rasqual_selected_pvalues.rds")
--- ATAC/QTL/rasqualTestIntections.R
+++ ATAC/QTL/rasqualTestIntections.R
@@ -11,14 +11,14 @@ atac_list = readRDS("results/ATAC/ATAC_combined_accessibility_data_covariates.rd
 gene_name_map = dplyr::select(atac_list$gene_metadata, gene_id, gene_name)
 
 #Import minimal p-values
-min_pvalue_list = readRDS("results/ATAC/QTLs/rasqual_min_pvalues.rds")
-min_pvalue_hits = lapply(min_pvalue_list, function(x){dplyr::filter(x, p_fdr < 0.1)})
+rasqual_min_pvalues = readRDS("results/ATAC/QTLs/rasqual_min_pvalues.rds")
+min_pvalue_hits = lapply(rasqual_min_pvalues, function(x){dplyr::filter(x, p_eigen < fdr_thresh)})
 min_pvalues_df = purrr::map_df(min_pvalue_hits, identity, .id = "condition_name") %>%
-  dplyr::group_by(gene_id) %>% dplyr::arrange(p_nominal) %>% ungroup()
+  dplyr::arrange(gene_id, p_nominal)
 joint_pairs = dplyr::select(min_pvalues_df, gene_id, snp_id) %>% unique()
 
 #Rasqual selected p-values
-rasqual_selected_results = readRDS("results/ATAC/QTLs/rasqual_selected_results.rds")
+rasqual_selected_results = readRDS("results/ATAC/QTLs/rasqual_selected_pvalues.rds")
 
 #Import the VCF file
 vcf_file = readRDS("../macrophage-gxe-study/genotypes/SL1344/imputed_20151005/imputed.86_samples.sorted.filtered.named.rds")
@@ -32,19 +32,20 @@ filtered_vcf = list(snpspos = snps_pos, genotypes = genotypes)
 filtered_pairs = filterHitsR2(joint_pairs, filtered_vcf$genotypes, .8)
 
 #### Test for interactions ####
-covariate_names = c("sex_binary", "cqn_PC1", "cqn_PC2", "cqn_PC3")
+covariate_names = c("sex_binary","short_long_ratio","assigned_frac","mt_frac")
 formula_qtl = as.formula(paste("expression ~ genotype + condition_name ", 
                                paste(covariate_names, collapse = " + "), sep = "+ "))
 formula_interaction = as.formula(paste("expression ~ genotype + condition_name + condition_name:genotype ", 
                                        paste(covariate_names, collapse = " + "), sep = "+ "))
 
 #Run model
-interaction_results = testMultipleInteractions(filtered_pairs, trait_matrix = atac_list$cqn, 
-                    sample_metadata = atac_list$sample_metadata, filtered_vcf, formula_qtl, formula_interaction)
-interaction_df = postProcessInteractionPvalues(interaction_results)
+interaction_results = testMultipleInteractions(filtered_pairs, trait_matrix = log(atac_list$tpm + 0.01, 2), 
+                    sample_metadata = atac_list$sample_metadata, filtered_vcf, formula_qtl, formula_interaction, id_field_separator = "-")
+interaction_df = postProcessInteractionPvalues(interaction_results, id_field_separator = "-")
 saveRDS(interaction_df, "results/ATAC/QTLs/rasqual_interaction_results.rds")
 interaction_df = readRDS("results/ATAC/QTLs/rasqual_interaction_results.rds")
 interaction_hits = dplyr::filter(interaction_df, p_fdr < 0.1)
+
 
 #Make a Q-Q plot for the interaction p-values
 qq_df = dplyr::mutate(interaction_df, p_eigen = p_nominal) %>% addExpectedPvalue()
@@ -54,8 +55,10 @@ qq_plot = ggplot(qq_df, aes(x = -log(p_expected,10), y = -log(p_nominal,10))) +
   theme_light() + 
   xlab("-log10 exptected p-value") + 
   ylab("-log10 observed p-value")
-ggsave("figures/supplementary/caQTL_interaction_Q-Q_plot.pdf", plot = qq_plot, width = 4, height = 4)
+ggsave("figures/supplementary/caQTL_interaction_Q-Q_plot_tpm.pdf", plot = qq_plot, width = 4, height = 4)
 
+#Quantify genomic inflation
+lambda = median(qchisq(1-interaction_df$p_nominal,1))/qchisq(0.5,1)
 
 #### Cluster effect sizes ####
 #Extract effect sizes for all gene-snp pairs from RASQUAL data
@@ -73,7 +76,7 @@ appear_cluster_means = calculateClusterMeans(appear_clusters)
 cluster_sizes = calculateClusterSizes(appear_clusters)
 
 #Reorder clusters
-cluster_reorder = data_frame(cluster_id = c(1,2,3,4,5,6), new_cluster_id = c(1,6,4,3,5,2))
+cluster_reorder = data_frame(cluster_id = c(1,2,3,4,5,6), new_cluster_id = c(4,3,2,1,6,5))
 
 #Make heatmap of effect sizes
 appear_betas = appear_clusters %>% dplyr::group_by(gene_id, snp_id) %>% 
@@ -116,10 +119,12 @@ disappear_cluster_means = calculateClusterMeans(disappear_clusters) %>%
 
 disappear_betas = disappear_clusters %>% dplyr::group_by(gene_id, snp_id) %>% dplyr::mutate(beta_scaled = beta/max(beta)) %>%
   dplyr::left_join(gene_name_map, by = "gene_id") %>%
-  dplyr::semi_join(disappear_cluster_sizes, by = "cluster_id") %>% ungroup()
+  dplyr::semi_join(disappear_cluster_sizes, by = "cluster_id") %>% 
+  ungroup() %>%
+  dplyr::left_join(figureNames(), by = "condition_name")
 
 ylabel = paste(sum(disappear_cluster_sizes$count), "caQTLs")
-dis_effect_size_heatmap = ggplot(disappear_betas, aes(x = condition_name, y = gene_name, fill = beta_scaled)) + 
+dis_effect_size_heatmap = ggplot(disappear_betas, aes(x = figure_name, y = gene_name, fill = beta_scaled)) + 
   facet_grid(cluster_id ~ .,  scales = "free_y", space = "free_y") + geom_tile() + 
   scale_x_discrete(expand = c(0, 0)) +
   scale_y_discrete(expand = c(0, 0)) +
@@ -128,36 +133,9 @@ dis_effect_size_heatmap = ggplot(disappear_betas, aes(x = condition_name, y = ge
   theme_grey() +
   theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(), 
         axis.title.x = element_blank(), axis.text.x=element_text(angle = 15)) +
-  theme(panel.margin = unit(0.2, "lines"))
+  theme(panel.spacing = unit(0.2, "lines"))
 ggsave("results/ATAC/QTLs/properties/caQTLs_disappear_kmeans_heatmap.png",dis_effect_size_heatmap, width = 4.5, height = 4)
 
 #Export variable qtls
 variable_qtls = list(appear = appear_betas, disappear = disappear_betas)
 saveRDS(variable_qtls, "results/ATAC/QTLs/rasqual_appear_disappear_qtls.rds")
-
-#Plot example
-plotEQTL("ATAC_peak_26312","rs789642", atac_list$cqn, vcf_file$genotypes, atac_list$sample_metadata, atac_list$gene_metadata)
-
-
-#### GWAS overlaps ####
-#Import GWAS catalog
-filtered_catalog = readRDS("../macrophage-gxe-study/annotations/gwas_catalog_v1.0.1-downloaded_2016-03-02.filtered.rds")
-
-#GWAS overlaps for QTLs that appear
-qtl_table = dplyr::select(interaction_hits, gene_id, snp_id)
-interaction_olaps = findGWASOverlaps(qtl_table, filtered_catalog, vcf_file)
-interaction_gwas_hits = dplyr::left_join(interaction_olaps, gene_name_map, by = "gene_id") %>%
-  dplyr::select(gene_name, snp_id, gwas_snp_id, R2, trait, gwas_pvalue)
-write.table(interaction_gwas_hits, "results/ATAC/eQTLs/appear_gwas_overlaps.txt", row.names = FALSE, sep = "\t", quote = FALSE)
-
-#All GWAS overlaps
-all_olaps = findGWASOverlaps(filtered_pairs, filtered_catalog, vcf_file, min_r2 = 0.8)
-all_gwas_hits = dplyr::left_join(all_olaps, gene_name_map, by = "gene_id") %>%
-  dplyr::select(gene_name, gene_id, snp_id, gwas_snp_id, R2, trait, gwas_pvalue)
-write.table(all_gwas_hits, "results/ATAC/QTLs/all_gwas_overlaps_R08.txt", row.names = FALSE, sep = "\t", quote = FALSE)
-
-#Rank traits by overlap size
-ranked_traits = rankTraitsByOverlapSize(dplyr::filter(all_gwas_hits, R2 > 0.8), filtered_catalog, min_overlap = 4)
-write.table(ranked_traits, "results/ATAC/QTLs/relative_gwas_overlaps_R08.txt", row.names = FALSE, sep = "\t", quote = FALSE)
-
-
