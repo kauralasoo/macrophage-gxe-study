0800af5b9a7d1aaa6680adada81e265732203db0
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-12-25 02:30:59 +0200

    Construct credible sets based on new FDRs

--- ATAC/finemapping/ATAC_construct_credible_sets.R
+++ ATAC/finemapping/ATAC_construct_credible_sets.R
@@ -1,35 +1,30 @@
 library("devtools")
 library("purrr")
 library("dplyr")
-load_all("../seqUtils/")
 library("MatrixEQTL")
-load_all("../macrophage-gxe-study/macrophage-gxe-study/housekeeping/")
 library("ggplot2")
+load_all("../seqUtils/")
 load_all("~/software/rasqual/rasqualTools/")
-
+load_all("../macrophage-gxe-study/macrophage-gxe-study/housekeeping/")
 
 #Import the VCF file
-vcf_file = readRDS("../macrophage-gxe-study/genotypes/SL1344/imputed_20151005/imputed.86_samples.sorted.filtered.named.rds")
+vcf_file = readRDS("genotypes/SL1344/imputed_20151005/imputed.86_samples.sorted.filtered.named.rds")
 
 #List of ATAC tabix files
-atac_tabix_list = list(naive = "../macrophage-chromatin/results/ATAC/rasqual/output/naive_100kb/naive_100kb.sorted.txt.gz",
-                       IFNg = "../macrophage-chromatin/results/ATAC/rasqual/output/IFNg_100kb/IFNg_100kb.sorted.txt.gz",
-                       SL1344 = "../macrophage-chromatin/results/ATAC/rasqual/output/SL1344_100kb/SL1344_100kb.sorted.txt.gz",
-                       IFNg_SL1344 = "../macrophage-chromatin/results/ATAC/rasqual/output/IFNg_SL1344_100kb/IFNg_SL1344_100kb.sorted.txt.gz")
+atac_tabix_list = qtlResults()$atac_rasqual
 
 #Import ATAC data
-atac_list = readRDS("../macrophage-chromatin/results/ATAC/ATAC_combined_accessibility_data.rds")
-min_pvalues_list = readRDS("../macrophage-chromatin/results/ATAC/QTLs/rasqual_min_pvalues.rds")
-min_pvalues_hits = lapply(min_pvalues_list, function(x){dplyr::filter(x, p_fdr < 0.1)})
-
+atac_list = readRDS("results/ATAC/ATAC_combined_accessibility_data.rds")
+min_pvalues_list = readRDS("results/ATAC/QTLs/rasqual_min_pvalues.rds")
+min_pvalues_hits = purrr::map(min_pvalues_list, ~dplyr::filter(., p_eigen < fdr_thresh))
 
 #Fetch p-values for each gene
 pvalues_list = purrr::map2(min_pvalues_hits, atac_tabix_list, function(x,y){
   rasqualTools::constructGeneRanges(x, atac_list$gene_metadata, cis_window = 5e4) %>%
     rasqualTools::tabixFetchGenes(y)
 })
-saveRDS(pvalue_list, "results/ATAC/QTLs/rasqual_QTL_pvalues.rds")
-pvalue_list = readRDS("results/ATAC/QTLs/rasqual_QTL_pvalues.rds")
+saveRDS(pvalues_list, "results/ATAC/QTLs/rasqual_QTL_pvalues.rds")
+pvalues_list = readRDS("results/ATAC/QTLs/rasqual_QTL_pvalues.rds")
 
 printGene <- function(x) {
   print(x$gene_id[1])
@@ -37,33 +32,25 @@ printGene <- function(x) {
   }
 
 #Construct credible sets
-naive_cs = purrr::map(pvalue_list$naive, ~dplyr::arrange(.,p_nominal) %>%
+naive_cs = purrr::map(pvalues_list$naive, ~dplyr::arrange(.,p_nominal) %>%
                        printGene() %>%
                        addR2FromLead(vcf_file$genotypes) %>% 
                        dplyr::filter(R2 > 0.8))
-saveRDS(naive_cs, "results/ATAC/QTLs/rasqual_naive_cs.rds")
 
-
-ifng_cs = purrr::map(pvalue_list$IFNg, ~dplyr::arrange(.,p_nominal) %>%
+ifng_cs = purrr::map(pvalues_list$IFNg, ~dplyr::arrange(.,p_nominal) %>%
                          printGene() %>%
                          addR2FromLead(vcf_file$genotypes) %>% 
                          dplyr::filter(R2 > 0.8))
-saveRDS(ifng_cs, "results/ATAC/QTLs/rasqual_IFNg_cs.rds")
 
-
-sl1344_cs = purrr::map(pvalue_list$SL1344, ~dplyr::arrange(.,p_nominal) %>%
+sl1344_cs = purrr::map(pvalues_list$SL1344, ~dplyr::arrange(.,p_nominal) %>%
                               printGene() %>%
                               addR2FromLead(vcf_file$genotypes) %>% 
                               dplyr::filter(R2 > 0.8))
-saveRDS(sl1344_cs, "results/ATAC/QTLs/rasqual_SL1344_cs.rds")
 
-
-ifng_sl1344_cs = purrr::map(pvalue_list$IFNg_SL1344, ~dplyr::arrange(.,p_nominal) %>%
+ifng_sl1344_cs = purrr::map(pvalues_list$IFNg_SL1344, ~dplyr::arrange(.,p_nominal) %>%
                        printGene() %>%
   addR2FromLead(vcf_file$genotypes) %>% 
   dplyr::filter(R2 > 0.8))
-saveRDS(ifng_sl1344_cs, "results/ATAC/QTLs/rasqual_IFNg_SL1344_cs.rds")
-
 
 #Save credible sets to disk
 credible_set_list = list(naive = naive_cs, IFNg = ifng_cs, SL1344 = sl1344_cs, IFNg_SL1344 = ifng_sl1344_cs)
