e3d2b5b9ac8a11f4dd532547cacc0dcdc95c78a7
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-01-14 15:41:43 +0000

    Export raw read counts for sharing

--- ATAC_RNA/exportCountsAndMetadata.R
+++ ATAC_RNA/exportCountsAndMetadata.R
@@ -7,8 +7,8 @@ line_data = readRDS("macrophage-gxe-study/data/covariates/compiled_line_metadata
   dplyr::select(-ips_culture_days, -passage_diff_bins)
 
 #Import ATAC and RNA datasets
-expression_list = readRDS("results/SL1344/combined_expression_data_covariates.rds")
-atac_list = readRDS("results/ATAC/ATAC_combined_accessibility_data_covariates.rds")
+expression_list = readRDS("results/SL1344/combined_expression_data.rds")
+atac_list = readRDS("results/ATAC/ATAC_combined_accessibility_data.rds")
 
 #Extract lines with good RNA-seq data
 rna_lines = dplyr::select(expression_list$sample_metadata, line_id, replicate) %>% 
@@ -29,3 +29,22 @@ compiled_data = dplyr::left_join(line_data, rna_lines, by = c("line_id","replica
 #Export line metadata
 write.table(compiled_data, "figures/tables/line_differentiation_metadata.txt", 
            sep = "\t", quote = FALSE, row.names = FALSE)
+
+#Export raw RNA-seq data counts
+write.table(expression_list$counts, "figures/tables/RNA_count_matrix.txt", sep = "\t", 
+            quote = FALSE, row.names = TRUE, col.names = TRUE)
+write.table(expression_list$sample_metadata, "figures/tables/RNA_sample_metadata.txt", sep = "\t", 
+            quote = FALSE, row.names = FALSE, col.names = TRUE)
+write.table(expression_list$gene_metadata, "figures/tables/RNA_gene_metadata.txt", sep = "\t", 
+            quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+#Export raw ATAC-seq data counts
+write.table(atac_list$counts, "figures/tables/ATAC_count_matrix.txt", sep = "\t", 
+            quote = FALSE, row.names = TRUE, col.names = TRUE)
+write.table(atac_list$sample_metadata, "figures/tables/ATAC_sample_metadata.txt", sep = "\t", 
+            quote = FALSE, row.names = FALSE, col.names = TRUE)
+write.table(atac_list$gene_metadata, "figures/tables/ATAC_gene_metadata.txt", sep = "\t", 
+            quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+
+
