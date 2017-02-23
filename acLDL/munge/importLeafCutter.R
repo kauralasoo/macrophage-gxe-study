9592cd6c7890f381c2dd2efe356dc6b55531c42a
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-02-24 00:24:20 +0200

    Add Ensembl gene ids to LeafCutter results

--- acLDL/munge/importLeafCutter.R
+++ acLDL/munge/importLeafCutter.R
@@ -7,6 +7,12 @@ load_all("../seqUtils/")
 ensembl_quants = readRDS("results/acLDL/acLDL_salmon_ensembl.rds")
 sample_names = colnames(ensembl_quants)
 
+#Import Ensembl transcript database
+txdb = loadDb("../../annotations/GRCh38/genes/Ensembl_87/TranscriptDb_GRCh38_87.db")
+exons = exonsBy(txdb, by = "tx", use.names = TRUE)
+transcript_data = tbl_df(readRDS("../../annotations/GRCh38/genes/Ensembl_87/Homo_sapiens.GRCh38.87.transcript_data.rds")) %>%
+  dplyr::select(ensembl_gene_id, ensembl_transcript_id)
+
 #Import intron and cluster counts from leafcutter
 intron_counts = read.table("processed/acLDL/leafcutter/leafcutter_perind_numers.counts.gz")[,sample_names]
 
@@ -17,7 +23,26 @@ intron_metadata = dplyr::data_frame(transcript_id = rownames(intron_counts)) %>%
   dplyr::group_by(gene_id) %>%
   dplyr::mutate(start = min(transcript_start), end = min(transcript_end)) %>%
   dplyr::mutate(strand = 1) %>%
-  dplyr::ungroup() %>%
+  dplyr::ungroup()
+
+#Find potential overlaps with Ensembl genes
+leafcutter_clusters = 
+  dplyr::transmute(intron_metadata, transcript_id, gene_id, 
+                   seqnames = chr, start = transcript_start, 
+                   end = transcript_end, strand = ifelse(strand == 1, "+", "-")) %>% 
+  dataFrameToGRanges()
+potential_overlaps = findOverlaps(leafcutter_clusters, exons, ignore.strand=TRUE)
+matches = data_frame(ensembl_transcript_id = names(exons[subjectHits(potential_overlaps)]), 
+                     gene_id = elementMetadata(leafcutter_clusters[queryHits(potential_overlaps)])$gene_id) %>%
+  dplyr::left_join(transcript_data, by = "ensembl_transcript_id") %>%
+  dplyr::select(gene_id, ensembl_gene_id) %>%
+  unique()
+grouped_matches = dplyr::group_by(matches, gene_id) %>%
+  dplyr::summarise(ensembl_gene_id = paste(ensembl_gene_id, collapse =";")) %>%
+  dplyr::ungroup()
+
+#Compile final intron metadata
+intron_metadata = dplyr::left_join(intron_metadata, grouped_matches, by = "gene_id") %>%
   as.data.frame()
 rownames(intron_metadata) = intron_metadata$transcript_id
 
