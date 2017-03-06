92432d4a174ab7db3d17a6c4144f1538bd9fe648
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-03-06 22:00:31 +0000

    Run coloc between eQTLs and caQTLs

--- ATAC_RNA/caQTLs_to_eQTLs/caQTL_eQTL_coloc.R
+++ ATAC_RNA/caQTLs_to_eQTLs/caQTL_eQTL_coloc.R
@@ -1,14 +1,29 @@
 library("devtools")
-library("plyr")
 library("dplyr")
 library("purrr")
-library("ggplot2")
+library("coloc")
+library("readr")
+library("optparse")
 load_all("../../seqUtils/")
-load_all("macrophage-gxe-study/housekeeping/")
-load_all("~/software/rasqual/rasqualTools/")
+
+#Parse command-line options
+option_list <- list(
+  make_option(c("-c", "--chr"), type="character", default=NULL,
+              help="Chromosome used for coloc analysis.", metavar = "type"),
+  make_option(c("-s", "--stimulation"), type="character", default=NULL,
+              help="Stimulation condition for coloc analysis.", metavar = "type"),
+  make_option(c("-o", "--outdir"), type="character", default=NULL,
+              help="Path to the output directory.", metavar = "type")
+)
+opt <- parse_args(OptionParser(option_list=option_list))
+
+#Extract options
+selected_chr = opt$c
+selected_condition = opt$s
+outdir = opt$o
 
 #Import R2 pairs of QTLs
-rna_atac_overlaps = readRDS("results/ATAC_RNA_overlaps/QTL_overlap_list_R2.rds")
+rna_atac_overlaps = readRDS("../results/ATAC_RNA_overlaps/QTL_overlap_list_R2.rds")
 
 #Import variant informaton
 GRCh38_variants = importVariantInformation("../genotypes/SL1344/imputed_20151005/imputed.86_samples.variant_information.txt.gz")
@@ -36,26 +51,27 @@ phenotype_list = list(
     QTLTools = FALSE)
 )
 
-rna_atac_overlaps
+#Extract parameters
+rna_summaries = phenotype_list$SL1344$qtl_summary_list[[selected_condition]]
+atac_summaries = phenotype_list$ATAC$qtl_summary_list[[selected_condition]]
+n_eqtl = phenotype_list$SL1344$sample_sizes[[selected_condition]]
+n_caqtl = phenotype_list$ATAC$sample_sizes[[selected_condition]]
 
+#Run coloc over all possible pairs
+res = rna_atac_overlaps %>% 
+  dplyr::filter(chr == selected_chr) %>%
+  dplyr::mutate(gene_id1 = gene_id, snp_id1 = snp_id) %>%
+  group_by(gene_id1, snp_id1) %>%
+  purrr::by_slice(function(slice){
+    gene_df = dplyr::transmute(slice, phenotype_id = gene_id, snp_id) %>% unique()
+    peak_df = dplyr::select(slice, peak_id) %>% unique()
+    coloc_res = colocGeneAgainstPeaks(gene_df, peak_df, eqtl_summaries = rna_summaries, 
+                                      caqtl_summaries = atac_summaries, n_eqtl = n_eqtl,
+                                      n_caqtl = n_caqtl, variant_information = GRCh38_variants)
+  })
 
-#Set test data
-gene_df = dplyr::filter(phenotype_list$SL1344$min_pvalues$IFNg, phenotype_id == "ENSG00000144228")
-peaks_df = dplyr::filter(rna_atac_overlaps, gene_id == "ENSG00000144228") %>% dplyr::select(peak_id) %>% unique()
-
-coloc_res = colocGeneAgainstPeaks(gene_df, peaks_df, qtlResults()$rna_fastqtl$IFNg, 
-                                  qtlResults()$atac_fastqtl$IFNg, n_eqtl = 84,
-                                  n_caqtl = 41, variant_information = GRCh38_variants)
-
-
-  
-  
-
-
-
-
-
-
-
-
+#Merge results together
+coloc_results = purrr::map_df(res$.out, identity)
+coloc_output = file.path(outdir, paste("eQTL_caQTL_coloc", selected_condition, selected_chr, "txt", sep = "."))
+write.table(coloc_results, coloc_output, sep = "\t", quote = FALSE, row.names = FALSE)
 
