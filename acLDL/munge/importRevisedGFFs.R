c6c3a7b695f980bf6acd541f462fdd72c81b5167
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-05-05 16:56:23 +0100

    Import revised GRanges

---
+++ acLDL/munge/importRevisedGFFs.R
@@ -0,0 +1,13 @@
+library("purrr")
+library("devtools")
+library("rtracklayer")
+load_all("../reviseAnnotations/")
+
+#Import revised GFF files
+gff_list = list(upstream = rtracklayer::import.gff3("processed/acLDL/annotations/gff/reviseAnnotations_upstream.gff3"),
+                contained = rtracklayer::import.gff3("processed/acLDL/annotations/gff/reviseAnnotations_contained.gff3"),
+                downstream = rtracklayer::import.gff3("processed/acLDL/annotations/gff/reviseAnnotations_downstream.gff3"))
+
+#Convert the GFF files into GRanges lists
+granges_lists = purrr::map(gff_list, ~reviseAnnotations::revisedGffToGrangesList(.))
+saveRDS(granges_list, "results/reviseAnnotations/reviseAnnotations.GRangesList.rds")
