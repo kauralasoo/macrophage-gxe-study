938ab8ad620bb5326926f9c5a7fe5800f75696c3
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-07-21 15:36:15 +0300

    Run coloc

--- Fairfax/run_Fairfax.sh
+++ Fairfax/run_Fairfax.sh
@@ -1,5 +1,5 @@
 #Process genotype data
-snakemake --cluster ../../../../macrophage-trQTLs/scripts/snakemake_submit.py -np -s process_genotypes.snakefile processed/Fairfax/merged_genotypes/fairfax_genotypes.variant_information.txt.gz --jobs 100
+snakemake --cluster ../../../../macrophage-trQTLs/scripts/snakemake_submit.py -np -s process_genotypes.snakefile processed/Fairfax/out.txt --jobs 100
 
 #Convert the VCF file into an R matrix
 bsub -G team170 -n1 -R "span[hosts=1] select[mem>35000] rusage[mem=35000]" -q normal -M 35000 -o importVCF.%J.jobout "/software/R-3.4.0/bin/Rscript macrophage-gxe-study/Fairfax/munge/importVCF.R"
@@ -8,5 +8,5 @@ bsub -G team170 -n1 -R "span[hosts=1] select[mem>35000] rusage[mem=35000]" -q no
 snakemake --cluster ../../../../macrophage-trQTLs/scripts/snakemake_submit.py -np -s map_QTLs.snakefile processed/Fairfax/out.txt --jobs 1200 --configfile fairfax_config.yaml
 
 #Run coloc against QTLs
-snakemake --cluster ../macrophage-trQTLs/scripts/snakemake_submit.py -np -s macrophage-gxe-study/fairfax_run_coloc.snakefile processed/Fairfax/coloc_out.txt --jobs 100 --configfile fairfax_config.yaml
+snakemake --cluster ../macrophage-trQTLs/scripts/snakemake_submit.py -np -s macrophage-gxe-study/fairfax_run_coloc.snakefile processed/Fairfax/coloc_out.txt --jobs 100 --configfile macrophage-gxe-study/Fairfax/QTLs/fairfax_config.yaml
 
