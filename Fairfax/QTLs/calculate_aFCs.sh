4d1dc64cbd3500f488f760cd20b619f26ef9c7ce
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-07-25 20:43:15 +0300

    Quantify false positives

--- Fairfax/QTLs/calculate_aFCs.sh
+++ Fairfax/QTLs/calculate_aFCs.sh
@@ -6,13 +6,13 @@ bgzip processed/Fairfax/qtltools/input/shared_84/LPS2.aFC.txt && tabix -p bed pr
 bgzip processed/Fairfax/qtltools/input/shared_84/LPS24.aFC.txt && tabix -p bed processed/Fairfax/qtltools/input/shared_84/LPS24.aFC.txt.gz
 
 #Run aFC calculation
-python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared_84/CD14.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared_84/qtl_pairs.txt --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared_84/CD14.aFC_results.txt
+python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared_84/CD14.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared_84/qtl_pairs.txt --boot 0 --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared_84/CD14.aFC_results.txt
 
-python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared_84/IFN.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared_84/qtl_pairs.txt --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared_84/IFN.aFC_results.txt
+python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared_84/IFN.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared_84/qtl_pairs.txt --boot 0 --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared_84/IFN.aFC_results.txt
 
-python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared_84/LPS2.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared_84/qtl_pairs.txt --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared_84/LPS2.aFC_results.txt
+python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared_84/LPS2.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared_84/qtl_pairs.txt --boot 0 --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared_84/LPS2.aFC_results.txt
 
-python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared_84/LPS24.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared_84/qtl_pairs.txt --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared_84/LPS24.aFC_results.txt
+python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared_84/LPS24.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared_84/qtl_pairs.txt --boot 0 --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared_84/LPS24.aFC_results.txt
 
 
 #Full dataset
@@ -22,3 +22,11 @@ bgzip processed/Fairfax/qtltools/input/shared/LPS2.aFC.txt && tabix -p bed proce
 bgzip processed/Fairfax/qtltools/input/shared/LPS24.aFC.txt && tabix -p bed processed/Fairfax/qtltools/input/shared/LPS24.aFC.txt.gz
 
 #Calculate aFC
+python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared/CD14.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared/qtl_pairs.txt --boot 0 --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared/CD14.aFC_results.txt
+
+python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared/IFN.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared/qtl_pairs.txt --boot 0 --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared/IFN.aFC_results.txt
+
+python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared/LPS2.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared/qtl_pairs.txt --boot 0 --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared/LPS2.aFC_results.txt
+
+python ~/software/aFC/aFC.py --vcf processed/Fairfax/merged_genotypes/fairfax_genotypes.sorted.filtered.vcf.gz --pheno processed/Fairfax/qtltools/input/shared/LPS24.aFC.txt.gz --qtl processed/Fairfax/qtltools/input/shared/qtl_pairs.txt --boot 0 --log_xform 1 --log_base 2 --o processed/Fairfax/qtltools/input/shared/LPS24.aFC_results.txt
+
