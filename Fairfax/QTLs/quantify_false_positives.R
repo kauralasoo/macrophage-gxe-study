bccf1c0e4e85bc819b60c81cf5d9b5a94facc8af
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-08-01 11:37:11 +0300

    Update FP analysis

--- Fairfax/QTLs/quantify_false_positives.R
+++ Fairfax/QTLs/quantify_false_positives.R
@@ -29,7 +29,7 @@ gene_name_map = rowData(se_shared) %>% tbl_df2() %>% dplyr::mutate(gene_id = pro
   dplyr::select(gene_id, gene_name)
 
 #Import results from interaction test
-shared_interactions = readRDS("results/Fairfax/shared_interactions_lme4.rds")
+shared_interactions = readRDS("results/Fairfax/interactions/shared_interactions_lme4.rds")
 shared_hits = dplyr::filter(shared_interactions, p_fdr < 0.1)
 
 #Import aFCs
@@ -45,11 +45,29 @@ shared_fc_hits_20 = dplyr::semi_join(shared_diff_df, shared_hits, by = c("gene_i
 
 
 ##### Import subset interaction results #####
-shared_84_interactions = readRDS("results/Fairfax/shared_84_interactions_lme4.rds")
+shared_84_interactions = readRDS("results/Fairfax/interactions/shared_84_interactions_lme4.rds")
 subset_hits = dplyr::filter(shared_84_interactions, p_fdr < 0.1)
 
 #Import aFCs
 subset_diff_df = importFairfaxAFC("processed/Fairfax/qtltools/input/shared_84/")
+
+#Filter by fold change
+subset_fc_hits = dplyr::semi_join(subset_diff_df, subset_hits, by = c("gene_id", "snp_id")) %>% 
+  dplyr::filter(abs(CD14) <= 0.29, max_fc >= 0.29, max_diff >= 0.29)
+
+#Estimate the rate of false positives
+length(intersect(subset_hits$gene_id, shared_hits$gene_id))/length(unique(subset_hits$gene_id))
+length(intersect(subset_fc_hits$gene_id, shared_fc_hits$gene_id))/length(unique(subset_fc_hits$gene_id))
+length(intersect(subset_fc_hits$gene_id, shared_fc_hits_20$gene_id))/length(unique(subset_fc_hits$gene_id))
+
+
+
+##### Import subset interaction results (n = 42) #####
+shared_42_interactions = readRDS("results/Fairfax/interactions/shared_42_interactions_lme4.rds")
+subset_hits = dplyr::filter(shared_84_interactions, p_fdr < 0.1)
+
+#Import aFCs
+subset_diff_df = importFairfaxAFC("processed/Fairfax/qtltools/input/shared_42/")
 
 #Filter by fold change
 subset_fc_hits = dplyr::semi_join(subset_diff_df, subset_hits, by = c("gene_id", "snp_id")) %>% 
